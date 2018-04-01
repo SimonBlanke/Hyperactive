eb722d9e188074199293f532dc4c7b5f0f92a559
Simon Blanke <simonblanke@hotmail.de>
2018-04-01 10:12:35 +0200

    code cleanup + add features
    argument to determine search time

--- random_search.py
+++ random_search.py
@@ -7,35 +7,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import multiprocessing
 
-
-from sklearn import datasets
-
-
-from dict1 import classifier_config_dict
-from dict1 import classifier_config_dict1
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-
 from importlib import import_module
-from sklearn.metrics import f1_score
-
-from sklearn.model_selection import cross_val_score
-
 from functools import partial
-
-iris = datasets.load_iris()
-X = iris.data[:, :2]
-y = iris.target
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+from sklearn.model_selection import cross_val_score
 
 
 num_cores = multiprocessing.cpu_count()
 print(num_cores, 'CPU threads available')
 
 
-def search(N_searches, X_train, y_train, scoring, ML_dict=classifier_config_dict1, cv=5):
+def random_search(N_searches, X_train, y_train, scoring, ML_dict, cv):
 
 	model = None
 	hyperpara_values = []
@@ -46,12 +27,9 @@ def search(N_searches, X_train, y_train, scoring, ML_dict=classifier_config_dict
 	hyperpara_dict = {}
 
 	dict1_len = len(ML_dict)
-	#print('dict1_len = ', dict1_len)
 	rand1 = random.randint(0, dict1_len-1)
-	#print('rand = ', rand1)
 
 	for i, key_model in zip(range(dict1_len), ML_dict.keys()):
-
 		if i == rand1:
 			model = key_model
 
@@ -75,53 +53,34 @@ def search(N_searches, X_train, y_train, scoring, ML_dict=classifier_config_dict
 
 		hyperpara_dict[hyperpara_name] = hyperpara_value
 
-		'''
-		for k, value in zip(range(dict3_len), dict1[model][key_hyperpara]):
 
-			print(key_hyperpara, ': ', value, '\n')
-
-			if k == rand2:
-				hyperpara_value.append(value)
-				
-		'''
 	sklearn, submod_func = model.rsplit('.', 1)
 	module = import_module(sklearn)
 	model = getattr(module, submod_func)
 
 	ML_model = model(**hyperpara_dict)
 
-	#print(ML_model)
-	#ML_model.fit(X_train, y_train)
-
-	scores = cross_val_score(ML_model, X, y, scoring=scoring, cv=cv)
+	scores = cross_val_score(ML_model, X_train, y_train, scoring=scoring, cv=cv)
 	score = scores.mean()
-
-	#y_pred = ML_model.predict(X_test)
-	#score = f1_score(y_test, y_pred, average='micro')
 
 	return ML_model, score
 
 
 
 
-def random_search(ML_dict, X_train, y_train, scoring, N_pipelines=100, T_search_time=None, cv=5):
+def multiprocessing_helper_func(ML_dict, X_train, y_train, scoring, cv, N_pipelines=100, T_search_time=None):
 
 	N_best_models = 1
 
 	pool = multiprocessing.Pool(num_cores)
-	search1 = partial(search, ML_dict=ML_dict, X_train=X_train, y_train=y_train, scoring=scoring, cv=cv)
-	models, scores = zip(*pool.map(search1, range(0, N_pipelines)))
+	random_search_obj = partial(random_search, ML_dict=ML_dict, X_train=X_train, y_train=y_train, scoring=scoring, cv=cv)
+	models, scores = zip(*pool.map(random_search_obj, range(0, N_pipelines)))
 
 	scores = np.array(scores)
 	index_best_scores = scores.argsort()[-N_best_models:][::-1]
 
 	best_score = scores[index_best_scores]
 	best_pipeline = models[index_best_scores[0]]
-
-	'''
-	for i in range(len(index_best_scores)):
-		print(models[index_best_scores[i]], '\n')
-	'''
 
 	return best_pipeline, best_score
 
@@ -131,15 +90,13 @@ def random_search(ML_dict, X_train, y_train, scoring, N_pipelines=100, T_search_
 def apply_random_search(ML_dict, X_train, y_train, scoring, N_pipelines=None, T_search_time=None, cv=5):
 	if T_search_time is not None and N_pipelines is None:
 		start = time.time()
-		random_search(ML_dict=ML_dict, X_train=X_train, y_train=y_train, scoring=scoring, N_pipelines=1000, T_search_time=None, cv=cv)
+		multiprocessing_helper_func(ML_dict=ML_dict, X_train=X_train, y_train=y_train, scoring=scoring, N_pipelines=1000, T_search_time=None, cv=cv)
 		search_time = time.time() - start
 
 		N_pipelines = int(1000 * T_search_time/search_time)
-		print(N_pipelines)
-
 
 		start = time.time()
-		best_pipeline, best_score = random_search(ML_dict=ML_dict, X_train=X_train, y_train=y_train, scoring=scoring, N_pipelines=N_pipelines, T_search_time=None, cv=cv)
+		best_pipeline, best_score = multiprocessing_helper_func(ML_dict=ML_dict, X_train=X_train, y_train=y_train, scoring=scoring, N_pipelines=N_pipelines, T_search_time=None, cv=cv)
 		print('search_time: ', time.time() - start)
 
 
@@ -148,9 +105,6 @@ def apply_random_search(ML_dict, X_train, y_train, scoring, N_pipelines=None, T_
 
 
 
-
-
-#apply_random_search(ML_dict=classifier_config_dict1, X_train=X, y_train=y, scoring='f1_micro', T_search_time=30, cv=3)
 
 
 
