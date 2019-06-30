6d0e8fac294475e7feda0b573e7e66096dd813ec
SimonBlanke <simonblanke@hotmail.de>
2019-06-30 12:31:53 +0200

    rename variables

--- hyperactive/particle_swarm_optimization.py
+++ hyperactive/particle_swarm_optimization.py
@@ -23,7 +23,7 @@ class ParticleSwarm_Optimizer(BaseOptimizer):
         verbosity=1,
         random_state=None,
         warm_start=False,
-        n_part=2,
+        n_part=4,
         w=0.5,
         c_k=0.5,
         c_s=0.9,
@@ -45,25 +45,25 @@ class ParticleSwarm_Optimizer(BaseOptimizer):
         self.c_k = c_k
         self.c_s = c_s
 
-    def _find_best_particle(self, cand, p_list):
-        for p in p_list:
+    def _find_best_particle(self, cand, part_list):
+        for p in part_list:
             if p.score_best > cand.score_best:
                 cand.score_best = p.score_best
                 cand.pos_best = p.pos_best
 
     def _init_particles(self, cand):
-        p_list = [Particle() for _ in range(self.n_part)]
-        for i, p in enumerate(p_list):
+        part_list = [Particle() for _ in range(self.n_part)]
+        for i, p in enumerate(part_list):
             p.nr = i
             p.pos = cand._space_.get_random_position()
             p.pos_best = p.pos
-            p.velo = np.zeros(self._get_dim_SearchSpace(cand))
+            p.velo = np.zeros(len(cand._space_.para_space))
 
-        return p_list
+        return part_list
 
-    def _move_particles(self, cand, p_list):
+    def _move_particles(self, cand, part_list):
 
-        for p in p_list:
+        for p in part_list:
             A = self.w * p.velo
             B = self.c_k * random.random() * np.subtract(p.pos_best, p.pos)
             C = self.c_s * random.random() * np.subtract(cand.pos_best, p.pos)
@@ -72,30 +72,28 @@ class ParticleSwarm_Optimizer(BaseOptimizer):
             p.velo = new_velocity
             p.move(cand)
 
-    def _eval_particles(self, cand, p_list, X_train, y_train):
-        for p in p_list:
+    def _eval_particles(self, cand, part_list, X, y):
+        for p in part_list:
             para = cand._space_.pos2para(p.pos)
-            p.score, _, p.sklearn_model = cand._model_.train_model(
-                para, X_train, y_train
-            )
+            p.score, _, _ = cand._model_.train_model(para, X, y)
 
             if p.score > p.score_best:
                 p.score_best = p.score
                 p.pos_best = p.pos
-
-    def _get_dim_SearchSpace(self, cand):
-        return len(cand._space_.para_space)
 
     def search(self, nth_process, X, y):
         _cand_ = self._init_search(nth_process, X, y)
 
         _cand_.eval(X, y)
 
-        p_list = self._init_particles(_cand_)
+        _cand_.score_best = _cand_.score
+        _cand_.pos_best = _cand_.pos
+
+        part_list = self._init_particles(_cand_)
         for i in tqdm.tqdm(**self._tqdm_dict(_cand_)):
-            self._eval_particles(_cand_, p_list, X, y)
-            self._find_best_particle(_cand_, p_list)
-            self._move_particles(_cand_, p_list)
+            self._eval_particles(_cand_, part_list, X, y)
+            self._find_best_particle(_cand_, part_list)
+            self._move_particles(_cand_, part_list)
 
         return _cand_
 
@@ -104,9 +102,9 @@ class Particle:
     def __init__(self):
         self.nr = None
         self.pos = None
-        self.score = 0
-
         self.pos_best = None
+
+        self.score = -1000
         self.score_best = -1000
 
         self.velo = None
@@ -114,5 +112,5 @@ class Particle:
     def move(self, cand):
         self.pos = (self.pos + self.velo).astype(int)
         # limit movement
-        n_zeros = [0] * len(cand._space_.n_values)
-        self.pos = np.clip(self.pos, n_zeros, cand._space_.n_values)
+        n_zeros = [0] * len(cand._space_.dim)
+        self.pos = np.clip(self.pos, n_zeros, cand._space_.dim)
