abde87d43ce690ece91af45bbfd780db7179be69
SimonBlanke <simonblanke@hotmail.de>
2019-07-04 16:30:28 +0200

    v0.3.2

--- hyperactive/__init__.py
+++ hyperactive/__init__.py
@@ -10,7 +10,7 @@ from .particle_swarm_optimization import ParticleSwarmOptimizer
 from .evolution_strategy import EvolutionStrategyOptimizer
 from .bayesian_optimization import BayesianOptimizer
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __license__ = "MIT"
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hyperactive",
-    version="0.3.1",
+    version="0.3.2",
     author="Simon Blanke",
     author_email="simon.blanke@yahoo.com",
     license="MIT",
