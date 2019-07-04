325114721091c237cae9ed6388473b47af7dbea6
SimonBlanke <simonblanke@hotmail.de>
2019-07-04 15:22:29 +0200

    add hill climbing

--- hyperactive/__init__.py
+++ hyperactive/__init__.py
@@ -2,10 +2,13 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+from .hill_climbing_optimizer import HillClimbingOptimizer
+
 from .random_search import RandomSearchOptimizer
 from .simulated_annealing import SimulatedAnnealingOptimizer
 from .particle_swarm_optimization import ParticleSwarmOptimizer
 from .evolution_strategy import EvolutionStrategyOptimizer
+from .bayesian_optimization import BayesianOptimizer
 
 __version__ = "0.3.1"
 __license__ = "MIT"
@@ -15,4 +18,5 @@ __all__ = [
     "SimulatedAnnealingOptimizer",
     "ParticleSwarmOptimizer",
     "EvolutionStrategyOptimizer",
+    "BayesianOptimizer",
 ]
