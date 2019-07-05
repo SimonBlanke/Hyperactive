91fa214e735373c9dd2842e6556c23ae9ebfa127
SimonBlanke <simonblanke@hotmail.de>
2019-07-05 16:32:29 +0200

    add stochastic hill climbing

---
+++ hyperactive/stochastic_hill_climbing.py
@@ -0,0 +1,85 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+import tqdm
+import random
+import numpy as np
+
+from .base import BaseOptimizer
+
+
+class StochasticHillClimbingOptimizer(BaseOptimizer):
+    def __init__(
+        self,
+        search_config,
+        n_iter,
+        metric="accuracy",
+        n_jobs=1,
+        cv=5,
+        verbosity=1,
+        random_state=None,
+        warm_start=False,
+        memory=True,
+        hyperband_init=False,
+        eps=1,
+        r=1e-6,
+    ):
+        super().__init__(
+            search_config,
+            n_iter,
+            metric,
+            n_jobs,
+            cv,
+            verbosity,
+            random_state,
+            warm_start,
+            memory,
+            hyperband_init,
+        )
+
+        self.eps = eps
+        self.r = r
+
+    def search(self, nth_process, X, y):
+        _cand_ = self._init_search(nth_process, X, y)
+        _climber_ = HillClimber(self.eps)
+
+        _cand_.eval(X, y)
+
+        _cand_.pos_current = _cand_.pos
+        _cand_.score_current = _cand_.score
+
+        for i in tqdm.tqdm(**self._tqdm_dict(_cand_)):
+            rand = random.uniform(0, self.r)
+
+            _climber_.climb(_cand_)
+            _cand_.eval(X, y)
+
+            score_diff_norm = abs(_cand_.score_current - _cand_.score) / (
+                _cand_.score_current + _cand_.score
+            )
+
+            if _cand_.score > _cand_.score_best:
+                _cand_.score_best = _cand_.score
+                _cand_.pos_best = _cand_.pos
+
+            elif score_diff_norm > rand:
+                _cand_.pos_current = _cand_.pos
+                _cand_.score_current = _cand_.score
+
+        return _cand_
+
+
+class HillClimber:
+    def __init__(self, eps):
+        self.eps = eps
+
+    def climb(self, _cand_, eps_mod=1):
+        sigma = (_cand_._space_.dim / 100) * self.eps * eps_mod
+        pos_new = np.random.normal(_cand_.pos_current, sigma, _cand_.pos.shape)
+        pos_new_int = np.rint(pos_new)
+
+        n_zeros = [0] * len(_cand_._space_.dim)
+        _cand_.pos = np.clip(pos_new_int, n_zeros, _cand_._space_.dim)
