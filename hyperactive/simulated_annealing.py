6863a8d420a2edbdbfe4a090ed82f40c6a61b6ac
SimonBlanke <simonblanke@hotmail.de>
2019-07-06 10:20:52 +0200

    put algorithm in separate method

--- hyperactive/simulated_annealing.py
+++ hyperactive/simulated_annealing.py
@@ -27,6 +27,7 @@ class SimulatedAnnealingOptimizer(BaseOptimizer):
         hyperband_init=False,
         eps=1,
         t_rate=0.98,
+        n_neighbours=1,
     ):
         super().__init__(
             search_config,
@@ -45,6 +46,27 @@ class SimulatedAnnealingOptimizer(BaseOptimizer):
         self.t_rate = t_rate
         self.temp = 0.1
 
+    def _annealing(self, _cand_):
+        self.temp = self.temp * self.t_rate
+        rand = random.uniform(0, 1)
+
+        # Normalized score difference to have a factor for later use with temperature and random
+        score_diff_norm = (self.score_current - _cand_.score) / (
+            self.score_current + _cand_.score
+        )
+
+        if _cand_.score > self.score_current:
+            self.score_current = _cand_.score
+            self.pos_curr = _cand_.pos
+
+            if _cand_.score > _cand_.score_best:
+                _cand_.score_best = _cand_.score
+                self.pos_curr = _cand_.pos
+
+        elif np.exp(-(score_diff_norm / self.temp)) > rand:
+            self.score_current = _cand_.score
+            self.hyperpara_indices_current = _cand_.pos
+
     def search(self, nth_process, X, y):
         _cand_ = self._init_search(nth_process, X, y)
         _annealer_ = Annealer()
@@ -57,28 +79,11 @@ class SimulatedAnnealingOptimizer(BaseOptimizer):
         self.score_current = _cand_.score
 
         for i in tqdm.tqdm(**self._tqdm_dict(_cand_)):
-            self.temp = self.temp * self.t_rate
-            rand = random.uniform(0, 1)
 
             _annealer_.find_neighbour(_cand_)
             _cand_.eval(X, y)
 
-            # Normalized score difference to have a factor for later use with temperature and random
-            score_diff_norm = (self.score_current - _cand_.score) / (
-                self.score_current + _cand_.score
-            )
-
-            if _cand_.score > self.score_current:
-                self.score_current = _cand_.score
-                self.pos_curr = _cand_.pos
-
-                if _cand_.score > _cand_.score_best:
-                    _cand_.score_best = _cand_.score
-                    self.pos_curr = _cand_.pos
-
-            elif np.exp(-(score_diff_norm / self.temp)) > rand:
-                self.score_current = _cand_.score
-                self.hyperpara_indices_current = _cand_.pos
+            self._annealing(_cand_)
 
         return _cand_
 
