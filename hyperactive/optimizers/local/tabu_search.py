86be08256e66a84e1d48d1fce262aadff8efcd68
SimonBlanke <simonblanke@hotmail.de>
2019-07-08 17:22:34 +0200

    first working draft

--- hyperactive/optimizers/local/tabu_search.py
+++ hyperactive/optimizers/local/tabu_search.py
@@ -8,10 +8,6 @@ import numpy as np
 
 from ...base import BaseOptimizer
 
-"""
-TODO
-"""
-
 
 class TabuOptimizer(BaseOptimizer):
     def __init__(
@@ -56,12 +52,17 @@ class TabuOptimizer(BaseOptimizer):
 
         for i in tqdm.tqdm(**self._tqdm_dict(_cand_)):
 
-            _climber_.climb(_cand_)
+            _climber_.climb_tabu(_cand_)
             _cand_.eval(X, y)
 
             if _cand_.score > _cand_.score_best:
                 _cand_.score_best = _cand_.score
                 _cand_.pos_best = _cand_.pos
+
+                _climber_.tabu_memory_short.append(_cand_.pos_best)
+
+                if len(_climber_.tabu_memory_short) > self.tabu_memory[0]:
+                    del _climber_.tabu_memory_short[0]
 
         return _cand_
 
@@ -70,10 +71,18 @@ class HillClimber:
     def __init__(self, eps):
         self.eps = eps
 
-    def climb(self, _cand_, eps_mod=1):
+        self.tabu_memory_short = []
+
+    def climb_tabu(self, _cand_, eps_mod=1):
         sigma = (_cand_._space_.dim / 100) * self.eps * eps_mod
-        pos_new = np.random.normal(_cand_.pos_best, sigma, _cand_.pos.shape)
-        pos_new_int = np.rint(pos_new)
+
+        in_tabu_mem = True
+        while in_tabu_mem:
+            pos_new = np.random.normal(_cand_.pos_best, sigma, _cand_.pos.shape)
+            pos_new_int = np.rint(pos_new)
+
+            if not any((pos_new_int == pos).all() for pos in self.tabu_memory_short):
+                in_tabu_mem = False
 
         n_zeros = [0] * len(_cand_._space_.dim)
         _cand_.pos = np.clip(pos_new_int, n_zeros, _cand_._space_.dim)
