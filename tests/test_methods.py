578250226e792355b77c7ff7010723a39fea7adc
SimonBlanke <simonblanke@hotmail.de>
2019-07-17 17:31:16 +0200

    try test with nosetests

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -4,22 +4,25 @@
 
 from hyperactive import RandomSearchOptimizer
 
-import pytest
+import unittest
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 
-def test_predict():
-    iris_data = load_iris()
-    X = iris_data.data
-    y = iris_data.target
+class TestOptimizerMethods(unittest.TestCase):
+    def test_predict(self):
+        iris_data = load_iris()
+        X = iris_data.data
+        y = iris_data.target
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-    search_config = {
-        "sklearn.ensemble.RandomForestClassifier": {"n_estimators": range(10, 100, 10)}
-    }
+        search_config = {
+            "sklearn.ensemble.RandomForestClassifier": {
+                "n_estimators": range(10, 100, 10)
+            }
+        }
 
-    Optimizer = RandomSearchOptimizer(search_config, n_iter=10, verbosity=0)
-    Optimizer.fit(X_train, y_train)
-    Optimizer.predict(X_test)
+        Optimizer = RandomSearchOptimizer(search_config, n_iter=10, verbosity=0)
+        Optimizer.fit(X_train, y_train)
+        Optimizer.predict(X_test)
