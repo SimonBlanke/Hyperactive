193f3f97c9868e66147b8be7613ca9a2e1c266df
SimonBlanke <simonblanke@hotmail.de>
2019-07-19 15:53:09 +0200

    removed imports

--- hyperactive/optimizers/population/__init__.py
+++ hyperactive/optimizers/population/__init__.py
@@ -3,16 +3,10 @@
 # License: MIT License
 
 from .particle_swarm_optimization import ParticleSwarmOptimizer
-from .spiral_optimization import SpiralOptimizer
 from .evolution_strategy import EvolutionStrategyOptimizer
-from .differential_evolution import DifferentialEvolutionOptimizer
-from .memetic_algorithm import MemeticOptimizer
 
 __all__ = [
     "ParticleSwarmOptimizer",
-    "SpiralOptimizer",
     "ParallelTemperingOptimizer",
     "EvolutionStrategyOptimizer",
-    "DifferentialEvolutionOptimizer",
-    "MemeticOptimizer",
 ]
