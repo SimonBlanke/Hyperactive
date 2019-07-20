5330fa0640c8c50dc4147d8803c541cd81de51c3
SimonBlanke <simonblanke@hotmail.de>
2019-07-20 12:16:03 +0200

    init commit

---
+++ tests/test_imports.py
@@ -0,0 +1,75 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+def test_import_HillClimbingOptimizer():
+    from hyperactive import HillClimbingOptimizer
+
+    _ = HillClimbingOptimizer  # avoids unused warning
+
+
+def test_import_StochasticHillClimbingOptimizer():
+    from hyperactive import StochasticHillClimbingOptimizer
+
+    _ = StochasticHillClimbingOptimizer  # avoids unused warning
+
+
+def test_import_TabuOptimizer():
+    from hyperactive import TabuOptimizer
+
+    _ = TabuOptimizer  # avoids unused warning
+
+
+def test_import_RandomSearchOptimizer():
+    from hyperactive import RandomSearchOptimizer
+
+    _ = RandomSearchOptimizer  # avoids unused warning
+
+
+def test_import_RandomRestartHillClimbingOptimizer():
+    from hyperactive import RandomRestartHillClimbingOptimizer
+
+    _ = RandomRestartHillClimbingOptimizer  # avoids unused warning
+
+
+def test_import_RandomAnnealingOptimizer():
+    from hyperactive import RandomAnnealingOptimizer
+
+    _ = RandomAnnealingOptimizer  # avoids unused warning
+
+
+def test_import_SimulatedAnnealingOptimizer():
+    from hyperactive import SimulatedAnnealingOptimizer
+
+    _ = SimulatedAnnealingOptimizer  # avoids unused warning
+
+
+def test_import_StochasticTunnelingOptimizer():
+    from hyperactive import StochasticTunnelingOptimizer
+
+    _ = StochasticTunnelingOptimizer  # avoids unused warning
+
+
+def test_import_ParallelTemperingOptimizer():
+    from hyperactive import ParallelTemperingOptimizer
+
+    _ = ParallelTemperingOptimizer  # avoids unused warning
+
+
+def test_import_ParticleSwarmOptimizer():
+    from hyperactive import ParticleSwarmOptimizer
+
+    _ = ParticleSwarmOptimizer  # avoids unused warning
+
+
+def test_import_EvolutionStrategyOptimizer():
+    from hyperactive import EvolutionStrategyOptimizer
+
+    _ = EvolutionStrategyOptimizer  # avoids unused warning
+
+
+def test_import_BayesianOptimizer():
+    from hyperactive import BayesianOptimizer
+
+    _ = BayesianOptimizer  # avoids unused warning
