0b362777d069032d540b2217f86d48b478a10b14
SimonBlanke <simonblanke@hotmail.de>
2019-07-26 16:51:09 +0200

    remove python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
