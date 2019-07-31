a7c909a6644dacc4e206fac5b183f99dae4797a2
SimonBlanke <simonblanke@hotmail.de>
2019-07-31 08:36:11 +0200

    v0.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hyperactive",
-    version="0.4.0",
+    version="0.4.1",
     author="Simon Blanke",
     author_email="simon.blanke@yahoo.com",
     license="MIT",
