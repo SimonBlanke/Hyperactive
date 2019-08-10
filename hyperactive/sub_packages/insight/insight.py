251853d0b370341e0911d656e96bda3fa14a296d
SimonBlanke <simonblanke@hotmail.de>
2019-08-10 21:17:52 +0200

    small fix

--- hyperactive/sub_packages/insight/insight.py
+++ hyperactive/sub_packages/insight/insight.py
@@ -17,24 +17,3 @@ class Insight:
 
     def _get_hash(self, object):
         return hashlib.sha1(object).hexdigest()
-
-
-data_hash_dict = {
-    "1c7f717bc3f902d5b72ba6735900b3454a0b96fa": "dict_path",
-    "2c7f717bc3f902d5b72ba6735900b3454a0b96fa": "dict_path",
-    "3c7f717bc3f902d5b72ba6735900b3454a0b96fa": "dict_path",
-}
-
-model_dict = {
-    "sklearn.ensemble.GradientBoostingClassifier": "dict_path",
-    "xgboost.XGBClassifier": "dict_path",
-    "torch": "dict_path",
-}
-
-    def _bla(self, data_hash, model_str):
-        meta_data_path = './meta_data/'+data_hash+'/'+model_str+'.csv'
-
-        meta_data = pd.to_csv(meta_data_path)
-
-
-data = pd.DataFrame(data, index="para_pos_string1", columns=[0.1, 0.2, ..., 10])
