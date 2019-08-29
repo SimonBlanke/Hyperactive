741b41e683a4e3a80b6abda8df0b2a59a081c66a
SimonBlanke <simonblanke@hotmail.de>
2019-08-29 08:37:12 +0200

    update examples

--- examples/example_basic.py
+++ examples/example_basic.py
@@ -3,12 +3,11 @@ from sklearn.datasets import load_iris
 from hyperactive import RandomSearchOptimizer
 
 iris_data = load_iris()
-X = iris_data.data
-y = iris_data.target
+X, y = iris_data.data, iris_data.target
 
 search_config = {
     "sklearn.ensemble.RandomForestClassifier": {"n_estimators": range(10, 100, 10)}
 }
 
-Optimizer = RandomSearchOptimizer(search_config, n_iter=10, verbosity=0)
-Optimizer.fit(X, y)
+opt = RandomSearchOptimizer(search_config, n_iter=10)
+opt.fit(X, y)
--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,8 +1,19 @@
 python example_basic.py
-python example_xgboost.py
-python example_multiprocessing.py
-python example_warm_start_sklearn.py
-python example_mlp.py
-python example_cnn.py
-python example_warm_start_keras.py
-python example_transfer_learning.py
+
+python ./machine_learning/sklearn_.py
+python ./machine_learning/xgboost_.py
+python ./machine_learning/lightgbm_.py
+python ./machine_learning/catboost_.py
+
+python ./deep_learning/mlp_classification.py
+python ./deep_learning/mlp_regression.py
+python ./deep_learning/cnn_mnist.py
+python ./deep_learning/cnn_cifar10.py
+
+python ./distribution/multiprocessing_.py
+
+python ./advanced_features/memory_.py
+python ./advanced_features/scatter_init.py
+python ./advanced_features/transfer_learning.py
+python ./advanced_features/warm_start_keras.py
+python ./advanced_features/warm_start_sklearn.py
