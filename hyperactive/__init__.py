da75caadec9449a50ddb7909c16f5c461e9054fa
SimonBlanke <simonblanke@hotmail.de>
2019-09-08 08:39:59 +0200

    v0.4.1.7

--- hyperactive/__init__.py
+++ hyperactive/__init__.py
@@ -2,7 +2,7 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
-__version__ = "0.4.1.6"
+__version__ = "0.4.1.7"
 __license__ = "MIT"
 
 from .optimizers.local import HillClimbingOptimizer
