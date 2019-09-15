be9f2b61415c882e34fe7a333580deab98a2c1a5
SimonBlanke <simonblanke@hotmail.de>
2019-09-15 07:20:38 +0200

    update

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,6 @@
 pytest test_packages.py -p no:warnings
 pytest test_data.py -p no:warnings
 pytest test_optimizers.py -p no:warnings
-pytest test_meta_learn.py -p no:warnings
+pytest test_performance.py -p no:warnings
+pytest test_arguments_api.py -p no:warnings
+# pytest test_meta_learn.py -p no:warnings
