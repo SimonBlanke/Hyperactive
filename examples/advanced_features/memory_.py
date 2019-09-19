ca1f66ac8086c5e7906cbe369956da2e6ce49ed2
Simon Blanke <simonblanke@hotmail.de>
2019-09-19 07:32:33 +0200

    Update memory_.py

--- examples/advanced_features/memory_.py
+++ examples/advanced_features/memory_.py
@@ -1,16 +1,27 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.datasets import load_iris
-from hyperactive import RandomSearchOptimizer
+from hyperactive import Hyperactive
 
 iris_data = load_iris()
 X = iris_data.data
 y = iris_data.target
 
-# this defines the model and hyperparameter search space
+def model(para, X_train, y_train):
+    model = GradientBoostingClassifier(
+        n_estimators=para["n_estimators"],
+        max_depth=para["max_depth"],
+        min_samples_split=para["min_samples_split"],
+    )
+    scores = cross_val_score(model, X_train, y_train, cv=3)
+
+    return scores.mean(), model
+
 search_config = {
-    "sklearn.ensemble.RandomForestClassifier": {
+    model: {
         "n_estimators": range(10, 200, 10),
-        "max_depth": [3, 4, 5, 6],
-        "criterion": ["gini", "entropy"],
+        "max_depth": range(2, 12),
+        "min_samples_split": range(2, 12),
     }
 }
 
@@ -19,7 +30,7 @@ The memory will remember previous evaluations done during the optimization proce
 Instead of retraining the model, it accesses the memory and uses the saved score/loss.
 This shows as a speed up during the optimization process, since the whole search space has been explored.
 """
-opt = RandomSearchOptimizer(search_config, n_iter=1000, memory=True)
+opt = Hyperactive(search_config, n_iter=1000, memory=True)
 
 # search best hyperparameter for given data
 opt.fit(X, y)
