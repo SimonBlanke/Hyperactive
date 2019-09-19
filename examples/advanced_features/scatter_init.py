c6b19f99c51bd9b3f2feaee2949c0358887d9115
Simon Blanke <simonblanke@hotmail.de>
2019-09-19 07:34:05 +0200

    Update scatter_init.py

--- examples/advanced_features/scatter_init.py
+++ examples/advanced_features/scatter_init.py
@@ -1,28 +1,39 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.datasets import load_iris
-from hyperactive import SimulatedAnnealingOptimizer
+from hyperactive import Hyperactive
 
 iris_data = load_iris()
 X = iris_data.data
 y = iris_data.target
 
-# this defines the model and hyperparameter search space
+def model(para, X_train, y_train):
+    model = GradientBoostingClassifier(
+        n_estimators=para["n_estimators"],
+        max_depth=para["max_depth"],
+        min_samples_split=para["min_samples_split"],
+    )
+    scores = cross_val_score(model, X_train, y_train, cv=3)
+
+    return scores.mean(), model
+
 search_config = {
-    "sklearn.ensemble.RandomForestClassifier": {
+    model: {
         "n_estimators": range(10, 200, 10),
-        "max_depth": [3, 4, 5, 6],
-        "criterion": ["gini", "entropy"],
+        "max_depth": range(2, 12),
+        "min_samples_split": range(2, 12),
     }
 }
 
 # Without scatter initialization
 opt = SimulatedAnnealingOptimizer(
-    search_config, n_iter=20, random_state=0, scatter_init=False
+    search_config, optimizer="HillClimbing", n_iter=20, random_state=0, scatter_init=False
 )
 opt.fit(X, y)
 
 
 # With scatter initialization
-opt = SimulatedAnnealingOptimizer(
-    search_config, n_iter=20, random_state=0, scatter_init=True
+opt = Hyperactive(
+    search_config, optimizer="HillClimbing", n_iter=20, random_state=0, scatter_init=True
 )
 opt.fit(X, y)
