01fb520b7b68bef9321ff2c6ccf75d088e3c3b8d
SimonBlanke <simonblanke@hotmail.de>
2019-09-21 18:42:21 +0200

    v0.5.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hyperactive",
-    version="0.4.2",
+    version="0.5.0",
     author="Simon Blanke",
     author_email="simon.blanke@yahoo.com",
     license="MIT",
@@ -38,7 +38,5 @@ setup(
         "xgboost",
         "lightgbm",
         "catboost",
-        "tensorflow",
-        "keras",
     ],
 )
