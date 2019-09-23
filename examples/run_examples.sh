83017820a4dadc2b9c4be940d5c50ff591aebfcb
SimonBlanke <simonblanke@hotmail.de>
2019-09-23 18:10:05 +0200

    add new examples

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,19 +1,16 @@
-python example_basic.py
+python ./machine_learning/sklearn_example.py
+python ./machine_learning/xgboost_example.py
+python ./machine_learning/lightgbm_example.py
+python ./machine_learning/catboost_example.py
+python ./machine_learning/sklearn_pipeline_example.py
+python ./machine_learning/stacking_example.py
 
-python ./machine_learning/sklearn_.py
-python ./machine_learning/xgboost_.py
-python ./machine_learning/lightgbm_.py
-python ./machine_learning/catboost_.py
-
-python ./deep_learning/mlp_classification.py
-python ./deep_learning/mlp_regression.py
-python ./deep_learning/cnn_mnist.py
+python ./deep_learning/mlp_example.py
 python ./deep_learning/cnn_cifar10.py
+python ./deep_learning/cnn_structure.py
 
-python ./distribution/multiprocessing_.py
+python ./distribution/multiprocessing_example.py
 
-python ./advanced_features/memory_.py
-python ./advanced_features/scatter_init.py
-python ./advanced_features/transfer_learning.py
-python ./advanced_features/warm_start_keras.py
-python ./advanced_features/warm_start_sklearn.py
+python ./extensions/memory_example.py
+python ./extensions/scatter_init_example.py
+python ./extensions/warm_start_example.py
