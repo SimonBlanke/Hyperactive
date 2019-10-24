61a8e69c67ae7bd6dc8e88e3226b2bbf7e61adea
Simon Blanke <simonblanke@hotmail.de>
2019-10-24 08:22:01 +0200

    Create himmelblau_example.py

---
+++ examples/test_functions/himmelblau_example.py
@@ -0,0 +1,21 @@
+import numpy as np
+from hyperactive import Hyperactive
+
+
+def himmelblau(para, X, y):
+    """Himmelblau's function"""
+
+    return -((para["x"]**2 + para["y"] - 11)**2 + (para["x"] + para["y"]**2 - 7)**2)
+
+
+x_range = np.arange(0, 10, 0.1)
+
+search_config = {
+    himmelblau: {
+        "x": x_range, 
+        "y": x_range
+    }
+}
+
+opt = Hyperactive(search_config, n_iter=1000000)
+opt.search(0, 0)
