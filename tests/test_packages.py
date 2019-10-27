ed3a6c9c3bbb0247413808a73f3aa5ba340b6ee3
SimonBlanke <simonblanke@hotmail.de>
2019-10-27 13:47:47 +0100

    test meta learn

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -11,6 +11,33 @@ data = load_breast_cancer()
 X, y = data.data, data.target
 
 
+def test_meta_learn():
+    from sklearn.tree import DecisionTreeClassifier
+
+    def model(para, X_train, y_train):
+        model = DecisionTreeClassifier(
+            criterion=para["criterion"],
+            max_depth=para["max_depth"],
+            min_samples_split=para["min_samples_split"],
+            min_samples_leaf=para["min_samples_leaf"],
+        )
+        scores = cross_val_score(model, X_train, y_train, cv=3)
+
+        return scores.mean()
+
+    search_config = {
+        model: {
+            "criterion": ["gini", "entropy"],
+            "max_depth": range(1, 21),
+            "min_samples_split": range(2, 21),
+            "min_samples_leaf": range(1, 21),
+        }
+    }
+
+    opt = Hyperactive(search_config, meta_learn=True)
+    opt.search(X, y)
+
+
 def test_sklearn():
     from sklearn.tree import DecisionTreeClassifier
 
