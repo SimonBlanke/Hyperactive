0167e31f04040760844f2dd09234c6a2b5137e8c
SimonBlanke <simonblanke@hotmail.de>
2019-11-04 18:52:31 +0100

    change formating

--- examples/use_cases/search_best_tree_ensemble.py
+++ examples/use_cases/search_best_tree_ensemble.py
@@ -1,7 +1,10 @@
 # disables sklearn warnings
 def warn(*args, **kwargs):
     pass
+
+
 import warnings
+
 warnings.warn = warn
 
 from sklearn.datasets import load_breast_cancer
@@ -20,32 +23,42 @@ X, y = data.data, data.target
 
 def GradientBoostingClassifier_(para, X, y):
     gbc = GradientBoostingClassifier(
-        n_estimators=para["n_estimators"], max_depth=para["max_depth"], min_samples_split=para["min_samples_split"], min_samples_leaf=para["min_samples_leaf"]
+        n_estimators=para["n_estimators"],
+        max_depth=para["max_depth"],
+        min_samples_split=para["min_samples_split"],
+        min_samples_leaf=para["min_samples_leaf"],
     )
     scores = cross_val_score(gbc, X, y, cv=3)
 
     return scores.mean()
 
+
 def RandomForestClassifier_(para, X, y):
     rfc = RandomForestClassifier(
-        n_estimators=para["n_estimators"], max_depth=para["max_depth"], min_samples_split=para["min_samples_split"], min_samples_leaf=para["min_samples_leaf"]
+        n_estimators=para["n_estimators"],
+        max_depth=para["max_depth"],
+        min_samples_split=para["min_samples_split"],
+        min_samples_leaf=para["min_samples_leaf"],
     )
     scores = cross_val_score(rfc, X, y, cv=3)
 
     return scores.mean()
 
+
 def ExtraTreesClassifier_(para, X, y):
     etc = ExtraTreesClassifier(
-        n_estimators=para["n_estimators"], max_depth=para["max_depth"], min_samples_split=para["min_samples_split"], min_samples_leaf=para["min_samples_leaf"]
+        n_estimators=para["n_estimators"],
+        max_depth=para["max_depth"],
+        min_samples_split=para["min_samples_split"],
+        min_samples_leaf=para["min_samples_leaf"],
     )
     scores = cross_val_score(etc, X, y, cv=3)
 
     return scores.mean()
 
+
 def XGBoost_(para, X, y):
-    etc = XGBClassifier(
-        n_estimators=para["n_estimators"], max_depth=para["max_depth"]
-    )
+    etc = XGBClassifier(n_estimators=para["n_estimators"], max_depth=para["max_depth"])
     scores = cross_val_score(etc, X, y, cv=3)
 
     return scores.mean()
@@ -70,11 +83,7 @@ search_config = {
         "min_samples_split": range(2, 20),
         "min_samples_leaf": range(2, 20),
     },
-    XGBoost_: {
-        "n_estimators": range(50, 300, 5),
-        "max_depth": range(2, 20),
-    },
-
+    XGBoost_: {"n_estimators": range(50, 300, 5), "max_depth": range(2, 20)},
 }
 
 
