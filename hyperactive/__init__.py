e635ae5f6c76b93a8c1d46d0678b2f5b56449cab
SimonBlanke <simonblanke@hotmail.de>
2019-11-06 09:34:58 +0100

    v1.1.2

--- hyperactive/__init__.py
+++ hyperactive/__init__.py
@@ -2,7 +2,7 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __license__ = "MIT"
 
 from .optimizers.local import HillClimbingOptimizer
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hyperactive",
-    version="1.1.1",
+    version="1.1.2",
     author="Simon Blanke",
     author_email="simon.blanke@yahoo.com",
     license="MIT",
