08ba54f349f7ce00d0558be68194b80a8e334a8f
SimonBlanke <simonblanke@hotmail.de>
2019-11-30 19:47:16 +0100

    update conf.py

--- docs/reference/conf.py
+++ docs/reference/conf.py
@@ -6,14 +6,19 @@ import os
 
 sys.path.insert(0, os.path.abspath("../../"))
 
-extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinx.ext.doctest"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+]
 source_suffix = ".rst"
 master_doc = "index"
 project = u"Hyperactive"
 copyright = u"Simon Blanke, simon.blanke@yahoo.com"
 exclude_patterns = ["_build"]
 pygments_style = "sphinx"
-html_theme = "default"
 autoclass_content = "both"
 html_theme = "sphinx_rtd_theme"
 html_sidebars = {}
