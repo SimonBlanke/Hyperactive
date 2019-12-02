2ca354a43e04928fa0aa35b814305aff57e2577b
SimonBlanke <simonblanke@hotmail.de>
2019-12-02 09:01:00 +0100

    add doc

--- hyperactive/hyperactive.py
+++ hyperactive/hyperactive.py
@@ -39,35 +39,41 @@ class Hyperactive:
     Optimization main class.
     """
 
-    def __init__(self, X, y, memory=True, random_state=1, verbosity=2, warnings=False):
+    def __init__(self, X, y, memory=True, random_state=1, verbosity=3, warnings=False):
         """
-        
+
         Parameters
         ----------
-        X: array-like or None
-            Training input samples used during the optimization process.
-            The training data is passed to the "X" argument in the objective function during the optimization process.
-            You can also pass "None" if you want to optimize an objective function that does not contain a machine learning model.
-        
-        y: array-like or None
-            Training target values used during the optimization process.
-            The target values are passed to the "y" argument in the objective function during the optimization process.
-            You can also pass "None" if you want to optimize an objective function that does not contain a machine learning model.
+            X: array-like or None
+                Training input samples used during the optimization process.
+                The training data is passed to the ``X`` argument in the objective function during the optimization process.
+                You can also pass ``None`` if you want to optimize an objective function that does not contain a machine learning model.
 
-        memory: bool, optional (default: True)
+            y: array-like or None
+                Training target values used during the optimization process.
+                The target values are passed to the ``y`` argument in the objective function during the optimization process.
+                You can also pass ``None`` if you want to optimize an objective function that does not contain a machine learning model.
 
-        random_state: int, optional (default: 1)
+            memory: bool, optional (default: True)
+                Collects and reuses evaluation information during optimization.
 
-        verbosity: int, optional (default: 2)
+            random_state: **int, optional (default: 1)**
 
-        warnings: bool, optional (default: False)
+                The seed of the pseudo random number generator.
 
-        
-        Returns
-        -------
-        None
-        
+            verbosity: **int, optional (default: 3)**
+                How much information Hyperactive provides during the optimization process
+
+                - 0 -> No information from Hyperactive.
+                - 1 -> Prints scores and positions of best evaluation after optimization.
+                - 2 -> Adds progress bar(s) with minimal information.
+                - 3 -> Adds current best score and iteration number of current best score to progress bar information.
+
+            warnings: **bool, optional (default: False)**
+                Disables warnings (like deprecation warnings) during optimization:
+                Warnings can be very intrusive, since they are often printed during each model evaluation.
         """
+        self.X = X
         self._main_args_ = MainArgs(X, y, memory, random_state, verbosity)
 
         if not warnings:
@@ -91,60 +97,36 @@ class Hyperactive:
     def search(
         self,
         search_config,
-        max_time=None,
         n_iter=10,
+        max_time=None,
         optimizer="RandomSearch",
         n_jobs=1,
         warm_start=False,
         scatter_init=False,
     ):
         """
-        run search
-        
+        Starts the optimization run.
+
         Parameters
         ----------
         search_config: dictionary
-            Defines the search space and links it to the objective function. 
+            Defines the search space and links it to the objective function.
             The objective function is the key of the dictionary, while the search space (which is also a dictionary) is the value.
             You can define multiple modeles/search-spaces in the search_config.
             The values within the search space (not search_config) must be lists or numpy arrays.
-            
-            Example:
-            def model_function(para, X, y):
-                model = GradientBoostingClassifier(
-                    n_estimators=para["n_estimators"],
-                    max_depth=para["max_depth"],
-                )
-                scores = cross_val_score(model, X, y, cv=3)
 
-                return scores.mean()
-
-
-            search_config = {
-                model_function: {
-                    "n_estimators": range(10, 200, 10),
-                    "max_depth": range(2, 12),
-                }
-            }
-            
-        
-        max_time: float, optional (default: None)
-        
         n_iter: int, optional (default: 10)
-        
+            Number of iterations.
+
+        max_time: float, optional (default: None)
+            Maximum time in hours to run the optimization.
+
         optimizer: string or dict, optional (default: "RandomSearch")
-        
+            Optimization strategy used during the run.
+
         n_jobs: int, optional (default: 1)
-        
-        warm_start: dict, optional (default: False)
-        
-        scatter_init: int, optional (default: False)
-        
-        
-        Returns
-        -------
-        None
-        
+            Number of jobs to run.
+
         """
 
         start_time = time.time()
