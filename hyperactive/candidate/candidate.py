5840acdaa4ad3a8c82aa4169be05ea75bf5b49a0
Simon Blanke <simonblanke@hotmail.de>
2019-12-11 13:19:31 +0100

    add long term memory

--- hyperactive/candidate/candidate.py
+++ hyperactive/candidate/candidate.py
@@ -2,32 +2,61 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+import numpy as np
+
 from ..search_space import SearchSpace
 from ..model import Model
 from ..init_position import InitSearchPosition
+from ..extentions import ShortTermMemory, LongTermMemory
 
 
 class Candidate:
+
     def __init__(self, nth_process, _main_args_):
         self.i = 0
         self.memory = _main_args_.memory
 
-        self._score_best = -1000
+        self._score_best = -np.inf
         self.pos_best = None
-
         self.model = None
 
         self.nth_process = nth_process
-
         model_nr = nth_process % _main_args_.n_models
         self.func_ = list(_main_args_.search_config.keys())[model_nr]
         self._space_ = SearchSpace(_main_args_, model_nr)
-
         self.func_name = str(self.func_).split(" ")[1]
-
         self._model_ = Model(self.func_, nth_process, _main_args_)
-
         self._init_ = InitSearchPosition(self._space_, self._model_, _main_args_)
+
+        if not self.memory:
+            self.mem = None
+            self.eval_pos = self.eval_pos_noMem
+
+            self.pos_best = _cand_._init_._set_start_pos()
+            self.score_best = _cand_.eval_pos(pos)
+            
+        elif self.memory == 'short':
+            self.mem = ShortTermMemory(self._space_, _main_args_)
+            self.eval_pos = self.eval_pos_Mem
+
+            self.pos_best = _cand_._init_._set_start_pos()
+            self.score_best = _cand_.eval_pos(pos)
+
+        elif self.memory == 'long':
+            self.mem = LongTermMemory(self._space_, _main_args_)
+            self.eval_pos = self.eval_pos_Mem
+
+            self.mem.load_memory(self.func_)
+            self.pos_best = self.mem.pos_best
+            self.score_best = self.mem.score_best
+
+        else:
+            print("Warning: Memory not defined")
+            self.mem = None
+            self.eval_pos = self.eval_pos_noMem
+
+            self.pos_best = _cand_._init_._set_start_pos()
+            self.score_best = _cand_.eval_pos(pos)
 
         self.eval_time_sum = 0
 
@@ -43,16 +72,26 @@ class Candidate:
         self.model_best = self.model
         self._score_best = value
 
-    def eval_pos(self, pos, force_eval=False):
+
+    def eval_pos_noMem(self, pos):
+        pos_str = pos.tostring()
+        para = self._space_.pos2para(pos)
+        para["iteration"] = self.i
+        score, eval_time, self.model = self._model_.train_model(para)
+        self.eval_time_sum = self.eval_time_sum + eval_time
+
+        return score
+
+    def eval_pos_Mem(self, pos, force_eval=False):
         pos_str = pos.tostring()
 
-        if pos_str in self._space_.memory and self.memory and not force_eval:
-            return self._space_.memory[pos_str]
+        if pos_str in self.mem.memory_dict and not force_eval:
+            return self.mem.memory_dict[pos_str]
         else:
             para = self._space_.pos2para(pos)
             para["iteration"] = self.i
             score, eval_time, self.model = self._model_.train_model(para)
-            self._space_.memory[pos_str] = score
+            self.mem.memory_dict[pos_str] = score
             self.eval_time_sum = self.eval_time_sum + eval_time
 
             return score
