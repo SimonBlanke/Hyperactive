6c50786b5837afafc88077374882acadf28cf4fa
SimonBlanke <simonblanke@hotmail.de>
2019-12-22 19:16:52 +0100

    change execute order

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-pytest test_packages.py -p no:warnings
+
 pytest test_optimizers.py -p no:warnings
 # pytest test_performance.py -p no:warnings
 pytest test_arguments_api.py -p no:warnings
 # pytest test_meta_learn.py -p no:warnings
+pytest test_packages.py -p no:warnings
