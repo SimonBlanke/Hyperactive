62ad0a1f23647174bfee2c33a3c635dc3e598529
SimonBlanke <simonblanke@hotmail.de>
2020-01-21 18:30:52 +0100

    remove python 3.8 from testing

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
