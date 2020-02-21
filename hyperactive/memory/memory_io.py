ac07896582b62463811fbaeaa8ff32a7fbc18960
SimonBlanke <simonblanke@hotmail.de>
2020-02-21 09:05:36 +0100

    rewrite methods as functions

--- hyperactive/memory/memory_io.py
+++ hyperactive/memory/memory_io.py
@@ -3,11 +3,8 @@
 # License: MIT License
 
 import os
-import glob
-import dill
 
-
-from .util import get_hash, get_model_id
+from .util import get_model_id, _hash2obj
 from .paths import (
     get_meta_path,
     get_model_path,
@@ -37,47 +34,4 @@ class MemoryIO:
         if not os.path.exists(self.date_path):
             os.makedirs(self.date_path, exist_ok=True)
 
-        self.hash2obj = self._hash2obj()
-
-    def _read_dill(self, value):
-        paths = self._get_pkl_hash(value)
-        for path in paths:
-            with open(path, "rb") as fp:
-                value = dill.load(fp)
-                value = dill.loads(value)
-                break
-
-        return value
-
-    def _get_pkl_hash(self, hash):
-        paths = glob.glob(self.model_path + hash + "*.pkl")
-
-        return paths
-
-    def _hash2obj(self):
-        hash2obj_dict = {}
-        para_hash_list = self._get_para_hash_list()
-
-        for para_hash in para_hash_list:
-            obj = self._read_dill(para_hash)
-            hash2obj_dict[para_hash] = obj
-
-        return hash2obj_dict
-
-    def _get_para_hash_list(self):
-        para_hash_list = []
-        for key in self._space_.search_space.keys():
-            values = self._space_.search_space[key]
-
-            for value in values:
-                if (
-                    not isinstance(value, int)
-                    and not isinstance(value, float)
-                    and not isinstance(value, str)
-                ):
-
-                    para_dill = dill.dumps(value)
-                    para_hash = get_hash(para_dill)
-                    para_hash_list.append(para_hash)
-
-        return para_hash_list
+        self.hash2obj = _hash2obj(_space_.search_space, self.model_path)
--- hyperactive/memory/util.py
+++ hyperactive/memory/util.py
@@ -2,6 +2,8 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+import glob
+import dill
 import inspect
 import hashlib
 
@@ -16,6 +18,53 @@ def get_hash(object):
 
 def get_model_id(model):
     return str(get_hash(get_func_str(model).encode("utf-8")))
+
+
+def _get_pkl_hash(hash, model_path):
+    paths = glob.glob(model_path + hash + "*.pkl")
+
+    return paths
+
+
+def _hash2obj(search_space, model_path):
+    hash2obj_dict = {}
+    para_hash_list = _get_para_hash_list(search_space)
+
+    for para_hash in para_hash_list:
+        obj = _read_dill(para_hash, model_path)
+        hash2obj_dict[para_hash] = obj
+
+    return hash2obj_dict
+
+
+def _read_dill(value, model_path):
+    paths = _get_pkl_hash(value, model_path)
+    for path in paths:
+        with open(path, "rb") as fp:
+            value = dill.load(fp)
+            value = dill.loads(value)
+            break
+
+    return value
+
+
+def _get_para_hash_list(search_space):
+    para_hash_list = []
+    for key in search_space.keys():
+        values = search_space[key]
+
+        for value in values:
+            if (
+                not isinstance(value, int)
+                and not isinstance(value, float)
+                and not isinstance(value, str)
+            ):
+
+                para_dill = dill.dumps(value)
+                para_hash = get_hash(para_dill)
+                para_hash_list.append(para_hash)
+
+    return para_hash_list
 
 
 """
