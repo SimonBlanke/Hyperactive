f88179267ce80385e577a97f2a583b3d34ffd5d6
SimonBlanke <simonblanke@hotmail.de>
2020-04-15 19:37:03 +0200

    move short term memory + hyperactive path for meta data

--- hyperactive/candidate.py
+++ hyperactive/candidate.py
@@ -2,13 +2,37 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+import os
 import time
 import numpy as np
 
 from .search_space import SearchSpace
 from .model import Model
 from .init_position import InitSearchPosition
-from hypermemory import ShortTermMemory, LongTermMemory
+
+from hypermemory import Hypermemory
+
+
+def meta_data_path():
+    current_path = os.path.realpath(__file__)
+    return current_path.rsplit("/", 1)[0] + "/meta_data/"
+
+
+class ShortTermMemory:
+    def __init__(self, _space_, _main_args_, _cand_):
+        self._space_ = _space_
+        self._main_args_ = _main_args_
+
+        self.pos_best = None
+        self.score_best = -np.inf
+
+        self.memory_type = _main_args_.memory
+        self.memory_dict = {}
+        self.memory_dict_new = {}
+
+        self.meta_data_found = False
+
+        self.n_dims = None
 
 
 class Candidate:
@@ -17,6 +41,9 @@ class Candidate:
         self.i = 0
         self._main_args_ = _main_args_
         self.memory = _main_args_.memory
+
+        self.memory_dict = {}
+        self.memory_dict_new = {}
 
         self._info_ = _info_()
 
@@ -41,46 +68,53 @@ class Candidate:
             self.mem = None
             self.eval_pos = self.eval_pos_noMem
 
+            self._init_eval()
+
         elif self.memory == "short":
-            self.mem = ShortTermMemory(self._space_, _main_args_, self)
+            self.mem = None
             self.eval_pos = self.eval_pos_Mem
+
+            self._init_eval()
 
         elif self.memory == "long":
-            self.mem = LongTermMemory(self._space_, _main_args_, self)
+            self.mem = Hypermemory(
+                _main_args_.X,
+                _main_args_.y,
+                self.func_,
+                self.search_space,
+                path=meta_data_path(),
+            )
             self.eval_pos = self.eval_pos_Mem
 
-            self.mem.load_memory(self, self._info_)
+            self.memory_dict = self.mem.load()
 
         else:
             print("Warning: Memory not defined")
             self.mem = None
             self.eval_pos = self.eval_pos_noMem
 
+            self._init_eval()
+
         if self.mem:
-            print("1")
             if self.mem.meta_data_found:
                 self.pos_best = self.mem.pos_best
                 self.score_best = self.mem.score_best
+            else:
+                self._init_eval()
 
-                print("self.score_best", self.score_best)
-
-        print("self.score_best", self.score_best)
-
+    def _init_eval(self):
         self.pos_best = self._init_._set_start_pos(self._info_)
-
-    def init_eval(self):
         self.score_best = self.eval_pos(self.pos_best)
 
     def _get_warm_start(self):
         return self._space_.pos2para(self.pos_best)
 
     def _process_results(self):
-
         self.total_time = time.time() - self.start_time
         start_point = self._info_.print_start_point(self)
 
         if self._main_args_.memory == "long":
-            self.mem.save_memory(self._main_args_, self)
+            self.mem.dump(self.memory_dict_new, main_args=self._main_args_)
 
         return start_point
 
@@ -109,10 +143,11 @@ class Candidate:
         pos.astype(int)
         pos_str = pos.tostring()
 
-        if pos_str in self.mem.memory_dict and not force_eval:
-            return self.mem.memory_dict[pos_str]["score"]
+        if pos_str in self.memory_dict and not force_eval:
+            return self.memory_dict[pos_str]["score"]
         else:
             results = self.base_eval(pos)
-            self.mem.memory_dict[pos_str] = results
-            self.mem.memory_dict_new[pos_str] = results
+            self.memory_dict[pos_str] = results
+            self.memory_dict_new[pos_str] = results
+
             return results["score"]
