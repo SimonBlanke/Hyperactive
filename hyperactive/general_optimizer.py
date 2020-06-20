8feb02869158afd684779f00bc8c2e15be975c61
SimonBlanke <simonblanke@hotmail.de>
2020-06-20 12:54:38 +0200

    set default kw-arguments

--- hyperactive/general_optimizer.py
+++ hyperactive/general_optimizer.py
@@ -15,6 +15,19 @@ def check_parameter(kwargs):
     pass
 
 
+def set_default(args, kwargs):
+    kwargs.setdefault("function_parameter", None)
+    kwargs.setdefault("memory", "long")
+    kwargs.setdefault("optimizer", "RandomSearch")
+    kwargs.setdefault("n_iter", 10)
+    kwargs.setdefault("n_jobs", 1)
+    kwargs.setdefault("random_state", None)
+    kwargs.setdefault("init", None)
+    kwargs.setdefault("distribution", None)
+
+    return kwargs
+
+
 class Optimizer:
     def __init__(
         self, verbosity=3, warnings=False, ext_warnings=False,
@@ -22,29 +35,22 @@ class Optimizer:
         self.verb = Verbosity(verbosity, warnings)
         self.search_processes = []
 
-    def add_search(
-        self,
-        **kwargs,
-        # obj_func,
-        # search_space,
-        # obj_func_para=None,
-        # memory="long",
-        # optimizer="RandomSearch",
-        # n_iter=10,
-        # n_jobs=1,
-        # random_state=1,
-        # init=None,
-        # distribution=None,
-    ):
-        # para_dict = check_parameter(args, kwargs)
-        para_dict = kwargs
-        self.n_jobs = para_dict["n_jobs"]
+    def add_search(self, *args, **kwargs):
+        # check_parameter(args, kwargs)
+        search_para = set_default(args, kwargs)
+        for arg in args:
+            if callable(arg):
+                search_para["objective_function"] = arg
+            elif isinstance(arg, dict):
+                search_para["search_space"] = arg
+
+        self.n_jobs = search_para["n_jobs"]
 
         for nth_process in range(self.n_jobs):
-            new_search_process = SearchProcess(para_dict, self.verb)
+            new_search_process = SearchProcess(search_para, self.verb)
             self.search_processes.append(new_search_process)
 
-        self.search = Search(self.search_processes, para_dict, self.n_jobs)
+        self.search = Search(self.search_processes, search_para, self.n_jobs)
 
     def run(self, max_time=None):
         self.search.run()
