2dc2a90dc38f76b873131795212ed759bee87069
SimonBlanke <simonblanke@hotmail.de>
2020-07-01 22:07:46 +0200

    refactor search process creation

--- hyperactive/general_optimizer.py
+++ hyperactive/general_optimizer.py
@@ -7,7 +7,6 @@ import time
 from importlib import import_module
 
 from .search import Search
-from .process_arguments import ProcessArguments
 from .verbosity import Verbosity
 
 
@@ -32,23 +31,62 @@ class Optimizer:
         self.hyperactive = hyperactive
         self.search_processes = []
 
-    def add_search(self, *args, **kwargs):
-        pro_arg = ProcessArguments(args, kwargs, random_state=self.random_state)
-
+    def _add_process(
+        self,
+        nth_job,
+        objective_function,
+        search_space,
+        n_iter,
+        function_parameter,
+        optimizer,
+        n_jobs,
+        memory,
+    ):
         module = import_module(".search_process", "hyperactive")
-        search_process_class = getattr(
-            module, search_process_dict[pro_arg.kwargs["memory"]]
-        )
+        search_process_class = getattr(module, search_process_dict[memory])
+
+        search_process_kwargs = {
+            "nth_job": nth_job,
+            "verb": self.verb,
+            "objective_function": objective_function,
+            "search_space": search_space,
+            "n_iter": n_iter,
+            "function_parameter": function_parameter,
+            "optimizer": optimizer,
+            "n_jobs": n_jobs,
+            "memory": memory,
+            "hyperactive": self.hyperactive,
+        }
+
+        new_search_process = search_process_class(**search_process_kwargs)
+        self.search_processes.append(new_search_process)
+
+    def add_search(
+        self,
+        objective_function,
+        search_space,
+        n_iter=10,
+        function_parameter=None,
+        optimizer="RandomSearch",
+        n_jobs=1,
+        memory="short",
+    ):
 
         for nth_job in range(pro_arg.n_jobs):
-            new_search_process = search_process_class(
-                nth_job, pro_arg, self.verb, hyperactive=self.hyperactive
+            self._add_process(
+                nth_job,
+                objective_function,
+                search_space,
+                n_iter,
+                function_parameter,
+                optimizer,
+                n_jobs,
+                memory,
             )
-            self.search_processes.append(new_search_process)
 
         self.search = Search(self.search_processes)
 
-    def run(self, max_time=None):
+    def run(self, max_time=None, distribution=None):
         if max_time is not None:
             max_time = max_time * 60
 
@@ -59,16 +97,6 @@ class Optimizer:
         self.position_results = self.search.position_results
         self.eval_times = self.search.eval_times
         self.iter_times = self.search.iter_times
-        # self.best_para = self.search.results
-        # self.best_score = self.search.results
+        self.best_para = self.search.results
+        self.best_score = self.search.results
 
-        """
-        self.results = dist.results
-        self.pos_list = dist.pos
-        # self.para_list = None
-        self.score_list = dist.scores
-
-        self.eval_times = dist.eval_times
-        self.iter_times = dist.iter_times
-        self.best_scores = dist.best_scores
-        """
