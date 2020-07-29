88a2c50a7d376cd1fafed45a336ac97b1ed8c55a
SimonBlanke <simonblanke@hotmail.de>
2020-07-29 10:34:49 +0200

    add print to show current test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,6 +8,7 @@ files = glob.glob("../examples/*/*.py")
 for file_path in files:
     file_name = str(file_path.rsplit("/", maxsplit=1)[1])
     try:
+        print("\033[0;33;40m Testing", file_name, end="...\r")
         subprocess.check_call(["python", file_path], stdout=DEVNULL, stderr=STDOUT)
     except subprocess.CalledProcessError:
         print("\033[0;31;40m Error in", file_name)
