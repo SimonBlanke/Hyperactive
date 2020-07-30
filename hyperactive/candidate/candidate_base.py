d6256a38b851380ec66c8e12e13ad81d56ca0357
SimonBlanke <simonblanke@hotmail.de>
2020-07-30 12:09:13 +0200

    always save positions/score in memory_dict

--- hyperactive/candidate/candidate_base.py
+++ hyperactive/candidate/candidate_base.py
@@ -31,6 +31,7 @@ class Candidate:
         self.para_best = None
 
         self.score_list = []
+        self.scores_best_list = []
         self.pos_list = []
 
         self.eval_times = []
@@ -65,6 +66,9 @@ class Candidate:
         para = self.space.pos2para(pos)
         results = self.model.eval(para)
 
+        self.score_list.append(results["score"])
+        self.scores_best_list.append(self.score_best)
+
         if results["score"] > self.score_best:
             self.score_best = results["score"]
             self.pos_best = pos
@@ -75,7 +79,13 @@ class Candidate:
         return results
 
     def eval_pos_noMem(self, pos, nth_iter):
+        pos.astype(int)
+        pos_tuple = tuple(pos)
+
         results = self.base_eval(pos, nth_iter)
+        if pos_tuple not in self.memory_dict_new:
+            self.memory_dict_new[pos_tuple] = results
+
         return results["score"]
 
     def eval_pos_Mem(self, pos, nth_iter, force_eval=False):
