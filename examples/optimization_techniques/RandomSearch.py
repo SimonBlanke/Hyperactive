fe95252fe1ea8865dbc422050d55b4d866a94528
SimonBlanke <simonblanke@hotmail.de>
2020-07-31 19:37:18 +0200

    add optimization examples

---
+++ examples/optimization_techniques/RandomSearch.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+
+from hyperactive import Hyperactive
+
+data = load_iris()
+X, y = data.data, data.target
+
+
+def model(para, X, y):
+    knr = KNeighborsClassifier(n_neighbors=para["n_neighbors"])
+    scores = cross_val_score(knr, X, y, cv=5)
+    score = scores.mean()
+
+    return score
+
+
+search_space = {
+    "n_neighbors": list(range(1, 100)),
+}
+
+optimizer = "RandomSearch"
+
+hyper = Hyperactive(X, y)
+hyper.add_search(model, search_space, optimizer=optimizer, n_iter=100)
+hyper.run()
+
