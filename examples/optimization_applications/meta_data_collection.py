f1eb1e830679fa49eb8b02470f4a5edeafb00c49
SimonBlanke <simonblanke@hotmail.de>
2020-08-09 18:31:53 +0200

    add meta-data-collection example

---
+++ examples/optimization_applications/meta_data_collection.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+
+from hyperactive import Hyperactive
+
+data = load_iris()
+X, y = data.data, data.target
+
+
+def model(para, X, y):
+    knr = KNeighborsClassifier(n_neighbors=para["n_neighbors"])
+    scores = cross_val_score(knr, X, y, cv=5)
+    score = scores.mean()
+
+    return score
+
+
+search_space = {
+    "n_neighbors": list(range(1, 100)),
+}
+
+
+"""
+- set memory to "long" to enable Hyperactive to collect and save data about the search
+"""
+hyper = Hyperactive(X, y)
+hyper.add_search(model, search_space, n_iter=100, memory="long")
+hyper.run()
+
+
+"""
+- when starting a new search Hyperactive will load the previously saved search data
+- loaded search data will be used to save computation time
+"""
+hyper = Hyperactive(X, y)
+hyper.add_search(model, search_space, n_iter=100, memory="long")
+hyper.run()
