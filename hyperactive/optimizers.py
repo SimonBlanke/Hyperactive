6b433175420bb50b756e3d813ef06620ebb4757c
SimonBlanke <simonblanke@hotmail.de>
2020-12-22 16:35:33 +0100

    add all available optimizers as wrapper classes

--- hyperactive/optimizers.py
+++ hyperactive/optimizers.py
@@ -9,16 +9,16 @@ from gradient_free_optimizers import (
     StochasticHillClimbingOptimizer as _StochasticHillClimbingOptimizer,
     RepulsingHillClimbingOptimizer as _RepulsingHillClimbingOptimizer,
     RandomSearchOptimizer as _RandomSearchOptimizer,
-    RandomRestartHillClimbingOptimizer,
-    RandomAnnealingOptimizer,
-    SimulatedAnnealingOptimizer,
-    ParallelTemperingOptimizer,
-    ParticleSwarmOptimizer,
-    EvolutionStrategyOptimizer,
-    BayesianOptimizer,
-    TreeStructuredParzenEstimators,
-    DecisionTreeOptimizer,
-    EnsembleOptimizer,
+    RandomRestartHillClimbingOptimizer as _RandomRestartHillClimbingOptimizer,
+    RandomAnnealingOptimizer as _RandomAnnealingOptimizer,
+    SimulatedAnnealingOptimizer as _SimulatedAnnealingOptimizer,
+    ParallelTemperingOptimizer as _ParallelTemperingOptimizer,
+    ParticleSwarmOptimizer as _ParticleSwarmOptimizer,
+    EvolutionStrategyOptimizer as _EvolutionStrategyOptimizer,
+    BayesianOptimizer as _BayesianOptimizer,
+    TreeStructuredParzenEstimators as _TreeStructuredParzenEstimators,
+    DecisionTreeOptimizer as _DecisionTreeOptimizer,
+    EnsembleOptimizer as _EnsembleOptimizer,
 )
 
 
@@ -137,4 +137,64 @@ class RandomSearchOptimizer(_BaseOptimizer_):
     def __init__(self, **opt_params):
         super().__init__(**opt_params)
         self._OptimizerClass = _RandomSearchOptimizer
+
+
+class RandomRestartHillClimbingOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _RandomRestartHillClimbingOptimizer
+
+
+class RandomAnnealingOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _RandomAnnealingOptimizer
+
+
+class SimulatedAnnealingOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _SimulatedAnnealingOptimizer
+
+
+class ParallelTemperingOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _ParallelTemperingOptimizer
+
+
+class ParticleSwarmOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _ParticleSwarmOptimizer
+
+
+class EvolutionStrategyOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _EvolutionStrategyOptimizer
+
+
+class BayesianOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _BayesianOptimizer
+
+
+class TreeStructuredParzenEstimators(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _TreeStructuredParzenEstimators
+
+
+class DecisionTreeOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _DecisionTreeOptimizer
+
+
+class EnsembleOptimizer(_BaseOptimizer_):
+    def __init__(self, **opt_params):
+        super().__init__(**opt_params)
+        self._OptimizerClass = _EnsembleOptimizer
 
