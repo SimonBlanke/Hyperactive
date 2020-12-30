22eb223ffef71ef1a188e26e1af743573a412856
SimonBlanke <simonblanke@hotmail.de>
2020-12-30 15:46:14 +0100

    remove unused files

--- examples/conv_examples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python conv_py2md.py machine_learning
-python conv_py2md.py deep_learning
-python conv_py2md.py distribution
-python conv_py2md.py extensions
-python conv_py2md.py test_functions
-python conv_py2md.py use_cases
