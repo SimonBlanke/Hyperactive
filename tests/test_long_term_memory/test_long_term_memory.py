960f9dc5a35b1e3ffd622ace73fdfbf2cb48fa56
SimonBlanke <simonblanke@hotmail.de>
2021-01-11 15:57:53 +0100

    add tests

--- tests/test_long_term_memory/test_long_term_memory.py
+++ tests/test_long_term_memory/test_long_term_memory.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+import pytest
 import numpy as np
 import pandas as pd
 
@@ -11,6 +12,99 @@ def remove_file(model_name):
         os.remove("./" + model_name + ".pkl")
     except OSError:
         pass
+
+
+def func1():
+    pass
+
+
+def func2():
+    pass
+
+
+def func3():
+    pass
+
+
+def objective_function(opt):
+    score = -opt["x1"] * opt["x1"]
+    return score
+
+
+class class1:
+    pass
+
+
+class class2:
+    pass
+
+
+class class3:
+    pass
+
+
+class class1_:
+    def __init__(self):
+        pass
+
+
+class class2_:
+    def __init__(self):
+        pass
+
+
+class class3_:
+    def __init__(self):
+        pass
+
+
+search_space_int = {
+    "x1": list(range(0, 3, 1)),
+}
+
+search_space_float = {
+    "x1": list(np.arange(0, 0.003, 0.001)),
+}
+
+search_space_str = {
+    "x1": list(range(0, 100, 1)),
+    "str1": ["0", "1", "2"],
+}
+
+search_space_func = {
+    "x1": list(range(0, 100, 1)),
+    "func1": [func1, func2, func3],
+}
+
+
+search_space_class = {
+    "x1": list(range(0, 100, 1)),
+    "class1": [class1, class2, class3],
+}
+
+
+search_space_obj = {
+    "x1": list(range(0, 100, 1)),
+    "class1": [class1_(), class2_(), class3_()],
+}
+
+search_space_lists = {
+    "x1": list(range(0, 100, 1)),
+    "list1": [[1, 1, 1], [1, 2, 1], [1, 1, 2]],
+}
+
+search_space_para = (
+    "search_space",
+    [
+        (search_space_int),
+        (search_space_float),
+        (search_space_str),
+        (search_space_func),
+        (search_space_class),
+        (search_space_obj),
+        (search_space_lists),
+    ],
+)
 
 
 def test_ltm_0():
@@ -38,16 +132,13 @@ def test_ltm_0():
     assert results1.equals(results2)
 
 
-def test_ltm_1():
+@pytest.mark.parametrize(*search_space_para)
+def test_ltm_1(search_space):
     model_name = "test_ltm_1"
 
     def objective_function(opt):
         score = -opt["x1"] * opt["x1"]
         return score
-
-    search_space = {
-        "x1": list(range(0, 3, 1)),
-    }
 
     memory = LongTermMemory(model_name, path="./")
 
