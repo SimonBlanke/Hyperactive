558f72628d55ba1fea39a5e1fe9ef809ed3104c1
Simon Blanke <simon.blanke@yahoo.com>
2021-01-20 19:36:43 +0100

    create new module "dashboard"

---
+++ hyperactive/long_term_memory/dashboard/__init__.py
@@ -0,0 +1,10 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+from .dashboard import Dashboard
+
+
+__all__ = [
+    "Dashboard",
+]
--- hyperactive/long_term_memory/dashboard/st_script.py
+++ hyperactive/long_term_memory/dashboard/st_script.py
@@ -72,8 +72,134 @@ def get_model_string(function):
     return inspect.getsource(function)
 
 
+def _score_statistics(search_data):
+    values_ = search_data["score"].values
+
+    mean_ = values_.mean()
+    std_ = values_.std()
+    min_ = np.amin(values_)
+    max_ = np.amax(values_)
+
+    df_data = pd.DataFrame(
+        [[mean_, std_, min_, max_]],
+        index=["score"],
+        columns=["mean", "std", "min", "max"],
+    )
+
+    col1, col2 = st.beta_columns(2)
+
+    col1.header("Score statistics")
+    col1.text("")
+    col2.text("")
+
+    col1.table(df_data)
+
+    fig = px.histogram(
+        search_data, x="score", nbins=int(len(search_data))
+    ).update_layout(width=1000, height=300)
+    col2.plotly_chart(fig)
+
+
+def _1d_scatter(search_data):
+    para_names = search_data.columns.drop("score")
+
+    st.text("")
+    col1, col2 = st.beta_columns(2)
+
+    col1.header("1D Scatter plot")
+    col1.text("")
+
+    scatter1_para1 = col1.selectbox(
+        "1D scatter plot parameter 1",
+        para_names,
+    )
+
+    fig = px.scatter(
+        search_data, x=scatter1_para1, y=search_data["score"]
+    ).update_layout(width=1000, height=600)
+    col2.plotly_chart(fig)
+
+
+def _2d_scatter(search_data):
+    para_names = search_data.columns.drop("score")
+
+    st.text("")
+    col1, col2 = st.beta_columns(2)
+
+    col1.header("2D Scatter plot")
+    col1.text("")
+
+    scatter2_para1 = col1.selectbox(
+        "2D scatter plot parameter 1",
+        para_names,
+    )
+    scatter2_para2 = col1.selectbox(
+        "2D scatter plot parameter 2",
+        para_names,
+    )
+
+    fig = px.scatter(
+        search_data, x=scatter2_para1, y=scatter2_para2, color="score"
+    ).update_layout(width=1000, height=600)
+    col2.plotly_chart(fig)
+
+
+def _3d_scatter(search_data):
+    para_names = search_data.columns.drop("score")
+
+    st.text("")
+    col1, col2 = st.beta_columns(2)
+
+    col1.header("3D Scatter plot")
+    col1.text("")
+
+    scatter3_para1 = col1.selectbox(
+        "3D scatter plot parameter 1",
+        para_names,
+    )
+    scatter3_para2 = col1.selectbox(
+        "3D scatter plot parameter 2",
+        para_names,
+    )
+    scatter3_para3 = col1.selectbox(
+        "3D scatter plot parameter 3",
+        para_names,
+    )
+
+    fig = px.scatter_3d(
+        search_data,
+        x=scatter3_para1,
+        y=scatter3_para2,
+        z=scatter3_para3,
+        color="score",
+    ).update_layout(width=1000, height=600)
+    col2.plotly_chart(fig)
+
+
+def _parallel_coordinates(search_data):
+    st.text("")
+    col1, col2 = st.beta_columns(2)
+
+    col1.header("Parallel Corrdinates")
+    col1.text("")
+    col2.text("")
+
+    xp = hip.Experiment.from_dataframe(search_data)
+    ret_val = xp.display_st(key="key1")
+
+
+plots_dict = {
+    "score_statistics": _score_statistics,
+    "1d_scatter": _1d_scatter,
+    "2d_scatter": _2d_scatter,
+    "3d_scatter": _3d_scatter,
+    "parallel_coordinates": _parallel_coordinates,
+}
+
+
 st.set_page_config(page_title="Hyperactive Dashboard", layout="wide")
 ltm_data_path = sys.argv[1]
+streamlit_plot_args = sys.argv[2:]
 
 
 st.title("Hyperactive Dashboard")
@@ -98,6 +224,7 @@ model_name_select = col1.selectbox(
     index=0,
 )
 
+# --- # paths
 model_study_path_ = search_data_path(
     ltm_data_path, model_name_select, study_name_select
 )
@@ -105,115 +232,20 @@ objective_function_path_ = objective_function_path(
     ltm_data_path, model_name_select, study_name_select
 )
 
+# --- # data readin
 search_data = dill_load(model_study_path_)
 objective_function_ = dill_load(objective_function_path_)
-model_string = get_model_string(objective_function_)
 
+
+# --- # show objective_function_ code
+model_string = get_model_string(objective_function_)
+col2.subheader("Objective function:")
+col2.code(model_string)
 
 if len(search_data) > 0:
-    para_names = search_data.columns.drop("score")
-
-    col2.subheader("Objective function:")
-    col2.code(model_string)
-
-    values_ = search_data["score"].values
-
-    mean_ = values_.mean()
-    std_ = values_.std()
-    min_ = np.amin(values_)
-    max_ = np.amax(values_)
-
-    df_data = pd.DataFrame(
-        [[mean_, std_, min_, max_]],
-        index=["score"],
-        columns=["mean", "std", "min", "max"],
-    )
-
-    # ----------------- # Score statistics
-    col1, col2 = st.beta_columns(2)
-
-    col1.header("Score statistics")
-    col1.text("")
-    col2.text("")
-
-    col1.table(df_data)
-
-    fig = px.histogram(
-        search_data, x="score", nbins=int(len(search_data))
-    ).update_layout(width=1000, height=300)
-    col2.plotly_chart(fig)
-
-    # ----------------- # N-dimensional Scatter plots
-    col1, col2 = st.beta_columns(2)
-
-    col1.header("N-dimensional Scatter plots")
-    col1.text("")
-    col2.text("")
-
-    # ----------------- # 1D scatter plot
-    col1, col2 = st.beta_columns(2)
-
-    scatter1_para1 = col1.selectbox(
-        "1D scatter plot parameter 1",
-        para_names,
-    )
-
-    fig = px.scatter(
-        search_data, x=scatter1_para1, y=search_data["score"]
-    ).update_layout(width=1000, height=600)
-    col2.plotly_chart(fig)
-
-    # ----------------- # 2D scatter plot
-    col1, col2 = st.beta_columns(2)
-
-    scatter2_para1 = col1.selectbox(
-        "2D scatter plot parameter 1",
-        para_names,
-    )
-    scatter2_para2 = col1.selectbox(
-        "2D scatter plot parameter 2",
-        para_names,
-    )
-
-    fig = px.scatter(
-        search_data, x=scatter2_para1, y=scatter2_para2, color="score"
-    ).update_layout(width=1000, height=600)
-    col2.plotly_chart(fig)
-
-    # ----------------- # 3D scatter plot
-    col1, col2 = st.beta_columns(2)
-
-    scatter3_para1 = col1.selectbox(
-        "3D scatter plot parameter 1",
-        para_names,
-    )
-    scatter3_para2 = col1.selectbox(
-        "3D scatter plot parameter 2",
-        para_names,
-    )
-    scatter3_para3 = col1.selectbox(
-        "3D scatter plot parameter 3",
-        para_names,
-    )
-
-    fig = px.scatter_3d(
-        search_data,
-        x=scatter3_para1,
-        y=scatter3_para2,
-        z=scatter3_para3,
-        color="score",
-    ).update_layout(width=1000, height=600)
-    col2.plotly_chart(fig)
-
-    # ----------------- # Parallel Corrdinates
-    col1, col2 = st.beta_columns(2)
-
-    col1.header("Parallel Corrdinates")
-    col1.text("")
-    col2.text("")
-
-    xp = hip.Experiment.from_dataframe(search_data)
-    ret_val = xp.display_st(key="key1")
+    # --- # create plots in order of "streamlit_plot_args"
+    for streamlit_plot_arg in streamlit_plot_args:
+        plots_dict[streamlit_plot_arg](search_data)
 
 else:
-    st.text("No search data found")
+    st.subheader("---> Error: Search data is empty!")
