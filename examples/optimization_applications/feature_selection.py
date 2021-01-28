61a17133b991a50a5f69b21e24739c04a46d3b50
Simon Blanke <simon.blanke@yahoo.com>
2021-01-28 14:46:22 +0100

    add doc to example for feature selection

--- examples/optimization_applications/feature_selection.py
+++ examples/optimization_applications/feature_selection.py
@@ -1,16 +1,28 @@
+"""
+This example shows how to select the best features for a model 
+and dataset. 
+
+The boston dataset has 13 features, therefore we have 13 search space 
+dimensions for the feature selection.
+
+The function "get_feature_indices" returns the list of features that
+where selected. This can be used to select the subset of features in "x_new".
+"""
+
 import numpy as np
 import itertools
 from sklearn.datasets import load_boston
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsRegressor
-from hyperactive import Hyperactive, HillClimbingOptimizer
+from hyperactive import Hyperactive, EvolutionStrategyOptimizer
 
 data = load_boston()
 X, y = data.data, data.target
 
 
-def model(opt):
-    feature_idx_list = []
+# helper function that returns the selected training data features by index
+def get_feature_indices(opt):
+    feature_indices = []
     for key in opt.keys():
         if "feature" not in key:
             continue
@@ -18,12 +30,17 @@ def model(opt):
             continue
 
         nth_feature = int(key.rsplit(".", 1)[1])
-        feature_idx_list.append(nth_feature)
+        feature_indices.append(nth_feature)
 
-    if len(feature_idx_list) == 0:
+    return feature_indices
+
+
+def model(opt):
+    feature_indices = get_feature_indices(opt)
+    if len(feature_indices) == 0:
         return 0
 
-    feature_idx_list = [idx for idx in feature_idx_list if idx is not None]
+    feature_idx_list = [idx for idx in feature_indices if idx is not None]
     x_new = X[:, feature_idx_list]
 
     knr = KNeighborsRegressor(n_neighbors=opt["n_neighbors"])
@@ -33,6 +50,7 @@ def model(opt):
     return score
 
 
+# each feature is used for training (True) or not used for training (False)
 search_space = {
     "n_neighbors": list(range(1, 100)),
     "feature.0": [True, False],
@@ -51,7 +69,14 @@ search_space = {
 }
 
 
-hyper = Hyperactive()
-hyper.add_search(model, search_space, n_iter=1500)
-hyper.run()
+optimizer = EvolutionStrategyOptimizer(rand_rest_p=0.20)
 
+hyper = Hyperactive()
+hyper.add_search(
+    model,
+    search_space,
+    n_iter=200,
+    initialize={"random": 15},
+    optimizer=optimizer,
+)
+hyper.run()
