0ed08fa806ad5af963fae86d8d8f7449e256c816
Simon Blanke <simon.blanke@yahoo.com>
2021-02-16 08:12:49 +0100

    update example

--- examples/optimization_applications/meta_data_collection.py
+++ examples/optimization_applications/meta_data_collection.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
@@ -8,31 +9,31 @@ data = load_iris()
 X, y = data.data, data.target
 
 
-def model(opt):
+def model1(opt):
     knr = KNeighborsClassifier(n_neighbors=opt["n_neighbors"])
-    scores = cross_val_score(knr, X, y, cv=5)
+    scores = cross_val_score(knr, X, y, cv=10)
     score = scores.mean()
 
     return score
 
 
-search_space = {
-    "n_neighbors": list(range(1, 100)),
-}
+search_space = {"n_neighbors": list(range(1, 50)), "leaf_size": list(range(5, 60, 5))}
 
 
-"""
-- set memory to "long" to enable Hyperactive to collect and save data about the search
-"""
 hyper = Hyperactive()
-hyper.add_search(model, search_space, n_iter=100, memory="long")
+hyper.add_search(model1, search_space, n_iter=500, memory=True)
 hyper.run()
 
+search_data = hyper.results(model1)
+# save the search data of a model for later use
+search_data.to_csv("./model1.csv", index=False)
 
-"""
-- when starting a new search Hyperactive will load the previously saved search data
-- loaded search data will be used to save computation time
-"""
+
+# load the search data and pass it to "memory_warm_start"
+search_data_loaded = pd.read_csv("./model1.csv")
+
 hyper = Hyperactive()
-hyper.add_search(model, search_space, n_iter=100, memory="long")
+hyper.add_search(
+    model1, search_space, n_iter=500, memory=True, memory_warm_start=search_data_loaded
+)
 hyper.run()
