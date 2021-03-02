3b228c90410fc57ad4f3ddc6e6251d46d694ad48
Stelios Tymvios <stelios.tymvios@icloud.com>
2021-03-03 00:38:37 +0200

    decouple number of runs from active processes

--- hyperactive/distribution.py
+++ hyperactive/distribution.py
@@ -7,23 +7,19 @@ from joblib import Parallel, delayed
 
 
 def single_process(process_func, search_processes_infos):
-    results = [process_func(**search_processes_infos[0])]
+    results = [process_func(**info) for info in search_processes_infos]
 
     return results
 
 
-def multiprocessing_wrapper(process_func, search_processes_paras, **kwargs):
-    n_jobs = len(search_processes_paras)
-
+def multiprocessing_wrapper(process_func, search_processes_paras, n_jobs=1, **kwargs):
     pool = Pool(n_jobs, **kwargs)
     results = pool.map(process_func, search_processes_paras)
 
     return results
 
 
-def joblib_wrapper(process_func, search_processes_paras, **kwargs):
-    n_jobs = len(search_processes_paras)
-
+def joblib_wrapper(process_func, search_processes_paras, n_jobs=1, **kwargs):
     jobs = [
         delayed(process_func)(**info_dict)
         for info_dict in search_processes_paras
