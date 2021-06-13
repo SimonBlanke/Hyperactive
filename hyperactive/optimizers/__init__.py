241927c4ffa749354d35b4623d6c7d2ec2349b61
Simon Blanke <simon.blanke@yahoo.com>
2021-06-13 16:55:09 +0200

    move optimizers to separate dir

---
+++ hyperactive/optimizers/__init__.py
@@ -0,0 +1,41 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+
+from .optimizers import (
+    HillClimbingOptimizer,
+    StochasticHillClimbingOptimizer,
+    RepulsingHillClimbingOptimizer,
+    RandomSearchOptimizer,
+    RandomRestartHillClimbingOptimizer,
+    RandomAnnealingOptimizer,
+    SimulatedAnnealingOptimizer,
+    ParallelTemperingOptimizer,
+    ParticleSwarmOptimizer,
+    EvolutionStrategyOptimizer,
+    BayesianOptimizer,
+    TreeStructuredParzenEstimators,
+    DecisionTreeOptimizer,
+    EnsembleOptimizer,
+)
+
+
+__all__ = [
+
+    "HillClimbingOptimizer",
+    "StochasticHillClimbingOptimizer",
+    "RepulsingHillClimbingOptimizer",
+    "RandomSearchOptimizer",
+    "RandomRestartHillClimbingOptimizer",
+    "RandomAnnealingOptimizer",
+    "SimulatedAnnealingOptimizer",
+    "ParallelTemperingOptimizer",
+    "ParticleSwarmOptimizer",
+    "EvolutionStrategyOptimizer",
+    "BayesianOptimizer",
+    "TreeStructuredParzenEstimators",
+    "DecisionTreeOptimizer",
+    "EnsembleOptimizer",
+]
