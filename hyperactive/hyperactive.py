64e18eaf4a45aebb98cece1959c8a0d52cb86a81
Simon Blanke <simon.blanke@yahoo.com>
2021-06-14 13:03:20 +0200

    separate hyperactive into two files

--- hyperactive/hyperactive.py
+++ hyperactive/hyperactive.py
@@ -3,89 +3,13 @@
 # License: MIT License
 
 
-import numpy as np
-import pandas as pd
 from tqdm import tqdm
 
 from .optimizers import RandomSearchOptimizer
 from .run_search import run_search
 from .print_info import print_info
 
-
-class HyperactiveResults:
-    def __init__(*args, **kwargs):
-        pass
-
-    def _sort_results_objFunc(self, objective_function):
-        best_score = -np.inf
-        best_para = None
-        search_data = None
-
-        results_list = []
-
-        for results_ in self.results_list:
-            nth_process = results_["nth_process"]
-
-            process_infos = self.process_infos[nth_process]
-            objective_function_ = process_infos["objective_function"]
-
-            if objective_function_ != objective_function:
-                continue
-
-            if results_["best_score"] > best_score:
-                best_score = results_["best_score"]
-                best_para = results_["best_para"]
-
-            results_list.append(results_["results"])
-
-        if len(results_list) > 0:
-            search_data = pd.concat(results_list)
-
-        self.objFunc2results[objective_function] = {
-            "best_para": best_para,
-            "best_score": best_score,
-            "search_data": search_data,
-        }
-
-    def _sort_results_search_id(self, search_id):
-        for results_ in self.results_list:
-            nth_process = results_["nth_process"]
-            search_id_ = self.process_infos[nth_process]["search_id"]
-
-            if search_id_ != search_id:
-                continue
-
-            best_score = results_["best_score"]
-            best_para = results_["best_para"]
-            search_data = results_["results"]
-
-            self.search_id2results[search_id] = {
-                "best_para": best_para,
-                "best_score": best_score,
-                "search_data": search_data,
-            }
-
-    def _get_one_result(self, id_, result_name):
-        if isinstance(id_, str):
-            if id_ not in self.search_id2results:
-                self._sort_results_search_id(id_)
-
-            return self.search_id2results[id_][result_name]
-
-        else:
-            if id_ not in self.objFunc2results:
-                self._sort_results_objFunc(id_)
-
-            return self.objFunc2results[id_][result_name]
-
-    def best_para(self, id_):
-        return self._get_one_result(id_, "best_para")
-
-    def best_score(self, id_):
-        return self._get_one_result(id_, "best_score")
-
-    def results(self, id_):
-        return self._get_one_result(id_, "search_data")
+from .hyperactive_results import HyperactiveResults
 
 
 class Hyperactive(HyperactiveResults):
@@ -113,11 +37,12 @@ class Hyperactive(HyperactiveResults):
         self.objFunc2results = {}
         self.search_id2results = {}
 
+        self.progress_paths = []
+
     def _add_search_processes(
         self,
         random_state,
         objective_function,
-        search_space,
         optimizer,
         n_iter,
         n_jobs,
@@ -134,7 +59,6 @@ class Hyperactive(HyperactiveResults):
                 "verbosity": self.verbosity,
                 "nth_process": nth_process,
                 "objective_function": objective_function,
-                "search_space": search_space,
                 "optimizer": optimizer,
                 "n_iter": n_iter,
                 "max_score": max_score,
@@ -160,19 +84,15 @@ class Hyperactive(HyperactiveResults):
         if isinstance(optimizer, str):
             if optimizer == "default":
                 optimizer = RandomSearchOptimizer()
-        optimizer.init(search_space, initialize)
 
-        if search_id is not None:
-            search_id = search_id
-            self.search_ids.append(search_id)
-        else:
-            search_id = str(len(self.search_ids))
-            self.search_ids.append(search_id)
+        if not search_id:
+            search_id = objective_function.__name__
+
+        optimizer.init(search_space, initialize)
 
         self._add_search_processes(
             random_state,
             objective_function,
-            search_space,
             optimizer,
             n_iter,
             n_jobs,
