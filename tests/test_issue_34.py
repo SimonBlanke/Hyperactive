67ae132b0f5642ce008dc4196baea83ffb424e6d
Simon Blanke <simon.blanke@yahoo.com>
2021-09-28 15:59:41 +0200

    fix test for issue 34

--- tests/test_issue_34.py
+++ tests/test_issue_34.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python3
-
-import pytest
 import numpy as np
-
 from hyperactive import Hyperactive
 
 
 def test_mixed_type_search_space():
-    """Test search spaces with mixed int/float types. Issue #34."""
-
     def objective_function(para):
         nonlocal para_types
         for v, t in zip(para.values(), para_types):
@@ -19,19 +13,24 @@ def test_mixed_type_search_space():
         return score
 
     search_space = {
-        "x1": list(range(10, 20)),
-        "x2": list(np.arange(1, 2, 0.1)),
+        "x1": range(10, 20),
+        "x2": np.arange(1, 2, 0.1),
     }
     para_types = [int, float]
     expected_pos = [1, 9]
 
     hyper = Hyperactive()
-    hyper.add_search(objective_function, search_space, n_iter=100)
+    hyper.add_search(objective_function, search_space, n_iter=10000)
     hyper.run()
 
-    best_para = hyper.best_para(objective_function)
     for best_para_val, expected_p, dim_space, p_type in zip(
-        best_para.values(), expected_pos, search_space.values(), para_types
+        hyper.best_para(objective_function).values(),
+        expected_pos,
+        search_space.values(),
+        para_types,
     ):
+        print("p_type", p_type)
+        print("dim_space", dim_space)
+
         assert best_para_val == dim_space[expected_p]
         assert isinstance(best_para_val, p_type)
