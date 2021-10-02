999c6e00484643f19e6651f57df2349731fb9a1c
Simon Blanke <simon.blanke@yahoo.com>
2021-10-02 12:16:37 +0200

    add more tests for issue 34

--- tests/test_issue_34.py
+++ tests/test_issue_34.py
@@ -1,8 +1,40 @@
 import numpy as np
 from hyperactive import Hyperactive
 
+""" --- test search spaces with mixed int/float types --- """
 
-def test_mixed_type_search_space():
+
+def test_mixed_type_search_space_0():
+    def objective_function(para):
+        assert isinstance(para["x1"], int)
+
+        return 1
+
+    search_space = {
+        "x1": list(range(10, 20)),
+    }
+
+    hyper = Hyperactive()
+    hyper.add_search(objective_function, search_space, n_iter=10000)
+    hyper.run()
+
+
+def test_mixed_type_search_space_1():
+    def objective_function(para):
+        assert isinstance(para["x2"], float)
+
+        return 1
+
+    search_space = {
+        "x2": list(np.arange(1, 2, 0.1)),
+    }
+
+    hyper = Hyperactive()
+    hyper.add_search(objective_function, search_space, n_iter=10000)
+    hyper.run()
+
+
+def test_mixed_type_search_space_2():
     def objective_function(para):
         assert isinstance(para["x1"], int)
         assert isinstance(para["x2"], float)
@@ -12,6 +44,27 @@ def test_mixed_type_search_space():
     search_space = {
         "x1": list(range(10, 20)),
         "x2": list(np.arange(1, 2, 0.1)),
+    }
+
+    hyper = Hyperactive()
+    hyper.add_search(objective_function, search_space, n_iter=10000)
+    hyper.run()
+
+
+def test_mixed_type_search_space_3():
+    def objective_function(para):
+        assert isinstance(para["x1"], int)
+        assert isinstance(para["x2"], float)
+        assert isinstance(para["x3"], float)
+        assert isinstance(para["x4"], str)
+
+        return 1
+
+    search_space = {
+        "x1": list(range(10, 20)),
+        "x2": list(np.arange(1, 2, 0.1)),
+        "x3": list(np.arange(1, 2, 0.1)),
+        "x4": ["str1", "str2", "str3"],
     }
 
     hyper = Hyperactive()
