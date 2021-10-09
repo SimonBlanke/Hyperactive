b586242a7f2e635ed732ab83260589deda06b660
Simon Blanke <simon.blanke@yahoo.com>
2021-10-09 19:15:24 +0200

    add tests for pathos multiprocessing

--- tests/test_memory/test_shared_memory.py
+++ tests/test_memory/test_shared_memory.py
@@ -447,3 +447,44 @@ def test_shared_memory_warm_start_3():
     d_time_2 = d_time_2 / 2
 
     assert d_time_1 / d_time_2 > 1.4
+
+
+def test_shared_memory_0():
+    def model_not_top_lvl(opt):
+        gbr = DecisionTreeRegressor(
+            min_samples_split=opt["min_samples_split"],
+        )
+        scores = cross_val_score(gbr, X, y, cv=cv)
+
+        return scores.mean()
+
+    c_time = time.time()
+    hyper = Hyperactive(n_processes=1)
+    hyper.add_search(
+        model_not_top_lvl,
+        search_space,
+        n_iter=100,
+        n_jobs=1,
+        memory="share",
+    )
+    hyper.run()
+    d_time_1 = time.time() - c_time
+
+    c_time = time.time()
+    hyper = Hyperactive(n_processes=1)
+    hyper.add_search(
+        model_not_top_lvl,
+        search_space,
+        n_iter=100,
+        n_jobs=4,
+        memory="share",
+    )
+    hyper.run()
+    d_time_2 = time.time() - c_time
+
+    print("\n d_time_1 \n", d_time_1)
+    print("\n d_time_2 \n", d_time_2)
+
+    d_time_2 = d_time_2 / 2
+
+    assert d_time_2 - d_time_1 < 0
