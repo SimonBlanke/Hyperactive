0cc0063553e572747674cb07b40ea0fb7809f996
Simon Blanke <simon.blanke@yahoo.com>
2021-12-02 12:42:25 +0100

    remove unused code

--- hyperactive/results.py
+++ hyperactive/results.py
@@ -49,38 +49,13 @@ class Results:
             "params": params,
         }
 
-    def _sort_results_search_id(self, search_id):
-        for results_ in self.results_list:
-            nth_process = results_["nth_process"]
-            search_id_ = self.process_infos[nth_process]["search_id"]
-
-            if search_id_ != search_id:
-                continue
-
-            best_score = results_["best_score"]
-            best_para = results_["best_para"]
-            search_data = results_["search_data"]
-
-            self.search_id2results[search_id] = {
-                "best_para": best_para,
-                "best_score": best_score,
-                "search_data": search_data,
-            }
-
     def _get_result(self, id_, result_name):
-        if isinstance(id_, str):
-            if id_ not in self.search_id2results:
-                self._sort_results_search_id(id_)
+        if id_ not in self.objFunc2results:
+            self._sort_results_objFunc(id_)
 
-            return self.search_id2results[id_][result_name]
+        search_data = self.objFunc2results[id_][result_name]
 
-        else:
-            if id_ not in self.objFunc2results:
-                self._sort_results_objFunc(id_)
-
-            search_data = self.objFunc2results[id_][result_name]
-
-            return search_data
+        return search_data
 
     def best_para(self, id_):
         best_para_ = self._get_result(id_, "best_para")
@@ -98,30 +73,10 @@ class Results:
 
         raise ValueError("objective function name not recognized")
 
-    def _create_mult_idx_search_data(self, search_data, params):
-        level_0 = []
-        level_1 = []
-        for para in params:
-            level_0.append("search_space")
-            level_1.append(para)
-        columns = list(search_data.columns)
-        results_cols = [x for x in columns if x not in params]
-
-        for results_col in results_cols:
-            level_0.append("search_data")
-            level_1.append(results_col)
-
-        arrays = [level_0, level_1]
-        mult_idx_tup = list(zip(*arrays))
-
-        index = pd.MultiIndex.from_tuples(mult_idx_tup, names=["type", "info"])
-        return pd.DataFrame(search_data.values, columns=index)
-
     def search_data(self, id_):
         search_data = self._get_result(id_, "search_data")
 
         params = self.objFunc2results[id_]["params"]
-        # search_data = self._create_mult_idx_search_data(search_data, params)
 
         if search_data is not None:
             return search_data
