18661e09f0a5f38817a1a5c6e7b022af8b8b6f00
Simon Blanke <simon.blanke@yahoo.com>
2022-04-30 19:48:18 +0200

    add tests to exception-handling

--- tests/test_catch.py
+++ tests/test_catch.py
@@ -76,7 +76,7 @@ def test_catch_3():
     hyper.run()
 
 
-def test_catch_all():
+def test_catch_all_0():
     def objective_function(access):
         x = y
         a = 1 + "str"
@@ -102,3 +102,35 @@ def test_catch_all():
     nan_ = hyper.search_data(objective_function)["score"].values[0]
 
     assert math.isnan(nan_)
+
+
+def test_catch_all_1():
+    def objective_function(access):
+        x = y
+        a = 1 + "str"
+        math.sqrt(-10)
+        x = 1 / 0
+
+        return 0, {"error": False}
+
+    catch_return = (np.nan, {"error": True})
+
+    hyper = Hyperactive()
+    hyper.add_search(
+        objective_function,
+        search_space,
+        n_iter=100,
+        catch={
+            NameError: catch_return,
+            TypeError: catch_return,
+            ValueError: catch_return,
+            ZeroDivisionError: catch_return,
+        },
+    )
+    hyper.run()
+
+    nan_ = hyper.search_data(objective_function)["score"].values[0]
+    error_ = hyper.search_data(objective_function)["error"].values[0]
+
+    assert math.isnan(nan_)
+    assert error_ == True
