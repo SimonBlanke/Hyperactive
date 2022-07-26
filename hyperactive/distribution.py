3c234ee9ab527fa02d4f385f3dd7a5e59660910d
Simon Blanke <simon.blanke@yahoo.com>
2022-07-26 18:27:35 +0200

    add ray support

--- hyperactive/distribution.py
+++ hyperactive/distribution.py
@@ -3,8 +3,23 @@
 # License: MIT License
 
 import multiprocessing as mp
-import pathos.multiprocessing as pmp
-from joblib import Parallel, delayed
+
+
+try:
+    import pathos.multiprocessing as pmp
+except ImportError:
+    pass
+
+try:
+    from joblib import Parallel, delayed
+except ImportError:
+    pass
+
+try:
+    import ray
+    from ray.util.multiprocessing import Pool
+except ImportError:
+    pass
 
 
 def single_process(process_func, process_infos):
@@ -30,5 +45,13 @@ def pathos_wrapper(process_func, search_processes_paras, n_processes, **kwargs):
 def joblib_wrapper(process_func, search_processes_paras, n_processes, **kwargs):
     jobs = [delayed(process_func)(*info_dict) for info_dict in search_processes_paras]
     results = Parallel(n_jobs=n_processes, **kwargs)(jobs)
+
+    return results
+
+
+def ray_wrapper(process_func, process_infos, n_processes, **kwargs):
+    # ray.init(log_to_driver=False)
+    pool = Pool(n_processes)
+    results = pool.map(process_func, process_infos)
 
     return results
