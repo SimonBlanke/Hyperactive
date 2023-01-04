0802b58d715a7ce46246c90be78fc897646e0487
Simon Blanke <simon.blanke@yahoo.com>
2023-01-04 20:47:48 +0100

    add memory tests for all optimizers

---
+++ tests/test_optimizers/test_memory.py
@@ -0,0 +1,86 @@
+import pytest
+import numpy as np
+
+
+from hyperactive import Hyperactive
+from ._parametrize import optimizers
+
+
+def objective_function(opt):
+    score = -opt["x1"] * opt["x1"]
+    return score
+
+
+def objective_function_m5(opt):
+    score = -(opt["x1"] - 5) * (opt["x1"] - 5)
+    return score
+
+
+def objective_function_p5(opt):
+    score = -(opt["x1"] + 5) * (opt["x1"] + 5)
+    return score
+
+
+search_space_0 = {"x1": list(np.arange(-100, 101, 1))}
+search_space_1 = {"x1": list(np.arange(0, 101, 1))}
+search_space_2 = {"x1": list(np.arange(-100, 1, 1))}
+
+search_space_3 = {"x1": list(np.arange(-10, 11, 0.1))}
+search_space_4 = {"x1": list(np.arange(0, 11, 0.1))}
+search_space_5 = {"x1": list(np.arange(-10, 1, 0.1))}
+
+search_space_6 = {"x1": list(np.arange(-0.0000000003, 0.0000000003, 0.0000000001))}
+search_space_7 = {"x1": list(np.arange(0, 0.0000000003, 0.0000000001))}
+search_space_8 = {"x1": list(np.arange(-0.0000000003, 0, 0.0000000001))}
+
+objective_para = (
+    "objective",
+    [
+        (objective_function),
+        (objective_function_m5),
+        (objective_function_p5),
+    ],
+)
+
+search_space_para = (
+    "search_space",
+    [
+        (search_space_0),
+        (search_space_1),
+        (search_space_2),
+        (search_space_3),
+        (search_space_4),
+        (search_space_5),
+        (search_space_6),
+        (search_space_7),
+        (search_space_8),
+    ],
+)
+
+
+@pytest.mark.parametrize(*objective_para)
+@pytest.mark.parametrize(*search_space_para)
+@pytest.mark.parametrize(*optimizers)
+def test_memory_0(Optimizer, search_space, objective):
+    optimizer = Optimizer()
+    search_space = search_space
+    objective_function = objective
+
+    n_iter = 30
+
+    hyper = Hyperactive()
+    hyper.add_search(
+        objective_function,
+        search_space,
+        optimizer=optimizer,
+        n_iter=n_iter,
+        n_jobs=2,
+    )
+    hyper.add_search(
+        objective_function,
+        search_space,
+        optimizer=optimizer,
+        n_iter=n_iter,
+        n_jobs=2,
+    )
+    hyper.run()
