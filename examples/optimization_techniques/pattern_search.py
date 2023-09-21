cae141f138518abe45e546ce3438e8f7e0e713ab
Simon Blanke <simon.blanke@yahoo.com>
2023-09-21 15:50:30 +0200

    add pattern-search example

---
+++ examples/optimization_techniques/pattern_search.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from hyperactive import Hyperactive
+from hyperactive.optimizers import PatternSearch
+
+
+def sphere_function(para):
+    x = para["x"]
+    y = para["y"]
+
+    return -(x * x + y * y)
+
+
+search_space = {
+    "x": list(np.arange(-10, 10, 0.1)),
+    "y": list(np.arange(-10, 10, 0.1)),
+}
+
+opt = PatternSearch(
+    n_positions=2,
+    pattern_size=0.5,
+    reduction=0.99,
+)
+
+
+hyper = Hyperactive()
+hyper.add_search(sphere_function, search_space, n_iter=1500, optimizer=opt)
+hyper.run()
