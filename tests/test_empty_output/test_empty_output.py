474e17e919e18af78fcfc31bcd397a4ea8d7494e
Simon Blanke <simon.blanke@yahoo.com>
2024-05-17 19:08:49 +0200

    change empty-output test

--- tests/test_empty_output/test_empty_output.py
+++ tests/test_empty_output/test_empty_output.py
@@ -19,5 +19,5 @@ def test_empty_output():
     print("\n verbose_str \n", verbose_str, "\n")
     print("\n non_verbose_str \n", non_verbose_str, "\n")
 
-    assert verbose_str
+    assert "Results:" in verbose_str
     assert not non_verbose_str
