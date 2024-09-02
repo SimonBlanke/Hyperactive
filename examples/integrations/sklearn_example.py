e98653ad7d19439dd808aa79a36acb3a8f05f97c
Simon Blanke <simon.blanke@yahoo.com>
2024-09-02 20:09:30 +0200

    add sklearn integration example

---
+++ examples/integrations/sklearn_example.py
@@ -0,0 +1,17 @@
+from sklearn import svm, datasets
+
+from hyperactive.integrations import HyperactiveSearchCV
+from sklearn.model_selection import GridSearchCV
+from hyperactive.optimizers import RandomSearchOptimizer
+
+iris = datasets.load_iris()
+
+
+svc = svm.SVC()
+opt = RandomSearchOptimizer()
+parameters = {"kernel": ["linear", "rbf"], "C": [1, 10]}
+
+search = HyperactiveSearchCV(svc, opt, parameters)
+search.fit(iris.data, iris.target)
+
+print("\n search.get_params() \n", search.get_params())
