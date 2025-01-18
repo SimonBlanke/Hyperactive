9c9c9ae753c307998646d7cf7cd5f621bd13d26a
Simon Blanke <simon.blanke@yahoo.com>
2025-01-18 10:33:06 +0100

    add stderr and text parameters

--- tests/test_empty_output/test_empty_output.py
+++ tests/test_empty_output/test_empty_output.py
@@ -8,16 +8,22 @@ non_verbose_file = os.path.join(here, "non_verbose.py")
 
 
 def test_empty_output():
-    output_verbose = subprocess.run(["python", verbose_file], stdout=subprocess.PIPE)
+    output_verbose = subprocess.run(
+        ["python", verbose_file],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
     output_non_verbose = subprocess.run(
         ["python", non_verbose_file], stdout=subprocess.PIPE
     )
 
-    verbose_str = output_verbose.stdout.decode()
-    non_verbose_str = output_non_verbose.stdout.decode()
+    verbose_str = output_verbose.stdout
+    non_verbose_str = output_non_verbose.stdout
 
     print("\n verbose_str \n", verbose_str, "\n")
     print("\n non_verbose_str \n", non_verbose_str, "\n")
 
     assert "Results:" in verbose_str
     assert not non_verbose_str
+    assert False
