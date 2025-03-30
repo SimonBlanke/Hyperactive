a10d915a3cf3c87375905110ff2b4cb1fa83380d
Simon Blanke <simon.blanke@yahoo.com>
2025-03-30 13:45:42 +0200

    small fix

--- src/hyperactive/experiment/__init__.py
+++ src/hyperactive/experiment/__init__.py
@@ -1,6 +1,6 @@
 """Base classes for optimizers and experiments."""
 
 from ._experiment import BaseExperiment
-from ._utility import add_callbacks
+from ._utility import add_callback, add_catch
 
-__all__ = ["BaseExperiment", "add_callbacks"]
+__all__ = ["BaseExperiment", "add_callback", "add_catch"]
--- src/hyperactive/experiment/_utility.py
+++ src/hyperactive/experiment/_utility.py
@@ -1,4 +1,4 @@
-def add_callbacks(before=None, after=None):
+def add_callback(before=None, after=None):
     def decorator(function):
         def wrapper(self, param):
             if before:
@@ -6,6 +6,21 @@ def add_callbacks(before=None, after=None):
             result = function(self, param)
             if after:
                 [after_callback(self, param) for after_callback in after]
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def add_catch(catch):
+    def decorator(function):
+        def wrapper(self, param):
+            try:
+                result = function(self, param)
+            except tuple(catch.keys()) as e:
+                result = catch[e.__class__]
+
             return result
 
         return wrapper
