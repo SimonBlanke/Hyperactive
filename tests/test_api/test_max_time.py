3dc50a1fe74fda3730248f546d897b595889c2c9
Simon Blanke <simon.blanke@yahoo.com>
2025-04-05 10:17:01 +0200

    add test_max_time.py

---
+++ tests/test_api/test_max_time.py
@@ -0,0 +1,38 @@
+import time
+import numpy as np
+from hyperactive.optimizers import HillClimbingOptimizer
+from hyperactive.experiment import BaseExperiment, add_catch
+from hyperactive.search_config import SearchConfig
+
+
+class Experiment(BaseExperiment):
+    def objective_function(self, para):
+        score = -para["x1"] * para["x1"]
+        return score
+
+
+experiment = Experiment()
+
+search_config = SearchConfig(
+    x1=list(np.arange(0, 100000, 1)),
+)
+
+
+def test_max_time_0():
+    c_time1 = time.perf_counter()
+    hyper = HillClimbingOptimizer()
+    hyper.add_search(experiment, search_config, n_iter=1000000)
+    hyper.run(max_time=0.1)
+    diff_time1 = time.perf_counter() - c_time1
+
+    assert diff_time1 < 1
+
+
+def test_max_time_1():
+    c_time1 = time.perf_counter()
+    hyper = HillClimbingOptimizer()
+    hyper.add_search(experiment, search_config, n_iter=1000000)
+    hyper.run(max_time=1)
+    diff_time1 = time.perf_counter() - c_time1
+
+    assert 0.3 < diff_time1 < 2
