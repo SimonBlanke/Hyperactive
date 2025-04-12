909b346d433193c1f421f851c101ac791fedc68a
Simon Blanke <simon.blanke@yahoo.com>
2025-04-12 10:54:29 +0200

    pass objective-function instead of experiment

--- src/hyperactive/optimizers/_optimizer_api.py
+++ src/hyperactive/optimizers/_optimizer_api.py
@@ -40,8 +40,10 @@ class BaseOptimizer(BaseObject):
         for key in search_space.keys():
             search_dim = search_space[key]
 
-            error_msg = "Value in '{}' of search space dictionary must be of type list".format(
-                key
+            error_msg = (
+                "Value in '{}' of search space dictionary must be of type list".format(
+                    key
+                )
             )
             if not isinstance(search_dim, list):
                 print("Warning", error_msg)
@@ -93,9 +95,7 @@ class BaseOptimizer(BaseObject):
         pass_through = pass_through or {}
         early_stopping = early_stopping or {}
 
-        search_id = self._default_search_id(
-            search_id, experiment.objective_function
-        )
+        search_id = self._default_search_id(search_id, experiment.objective_function)
         s_space = SearchSpace(search_space)
         self.verbosity = verbosity
 
@@ -135,12 +135,12 @@ class BaseOptimizer(BaseObject):
         self.comp_opt = CompositeOptimizer(self)
         self.comp_opt.run(max_time, distribution, n_processes, self.verbosity)
 
-    def best_para(self, id_):
+    def best_para(self, experiment):
         """
         Retrieve the best parameters for a specific ID from the results.
 
         Parameters:
-        - id_ (int): The ID of the parameters to retrieve.
+        - experiment (int): The experiment of the optimization run.
 
         Returns:
         - Union[Dict[str, Union[int, float]], None]: The best parameters for the specified ID if found, otherwise None.
@@ -149,33 +149,31 @@ class BaseOptimizer(BaseObject):
         - ValueError: If the objective function name is not recognized.
         """
 
-        return self.comp_opt.results_.best_para(id_)
+        return self.comp_opt.results_.best_para(experiment.objective_function)
 
-    def best_score(self, id_):
+    def best_score(self, experiment):
         """
         Return the best score for a specific ID from the results.
 
         Parameters:
-        - id_ (int): The ID for which the best score is requested.
+        - experiment (int): The experiment of the optimization run.
         """
 
-        return self.comp_opt.results_.best_score(id_)
+        return self.comp_opt.results_.best_score(experiment.objective_function)
 
-    def search_data(self, id_, times=False):
+    def search_data(self, experiment, times=False):
         """
         Retrieve search data for a specific ID from the results. Optionally exclude evaluation and iteration times if 'times' is set to False.
 
         Parameters:
-        - id_ (int): The ID of the search data to retrieve.
+        - experiment (int): The experiment of the optimization run.
         - times (bool, optional): Whether to exclude evaluation and iteration times. Defaults to False.
 
         Returns:
         - pd.DataFrame: The search data for the specified ID.
         """
 
-        search_data_ = self.comp_opt.results_.search_data(
-            id_.objective_function
-        )
+        search_data_ = self.comp_opt.results_.search_data(experiment.objective_function)
 
         if times == False:
             search_data_.drop(
