32f948a8d8f6afc47e8f72b065a0ab25e68cd29a
Franz Király <fkiraly@gcos.ai>
2025-05-04 00:40:16 +0200

    Update __init__.py

--- src/hyperactive/experiment/__init__.py
+++ src/hyperactive/experiment/__init__.py
@@ -1,6 +1,5 @@
 """Base classes for optimizers and experiments."""
 
 from hyperactive.base import BaseExperiment
-from ._utility import add_callback, add_catch
 
-__all__ = ["BaseExperiment", "add_callback", "add_catch"]
+__all__ = ["BaseExperiment"]
