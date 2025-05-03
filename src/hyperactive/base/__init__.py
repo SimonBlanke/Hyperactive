2e885135f80af7671433bd7e63df26b1f655b8ed
Franz Király <fkiraly@gcos.ai>
2025-05-03 16:37:51 +0200

    base

---
+++ src/hyperactive/base/__init__.py
@@ -0,0 +1,6 @@
+"""Base classes for optimizers and experiments."""
+
+from hyperactive.base._experiment import BaseExperiment
+from hyperactive.base._optimizer import BaseOptimizer
+
+__all__ = ["BaseExperiment", "BaseOptimizer"]
