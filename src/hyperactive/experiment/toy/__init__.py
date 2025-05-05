40a62bb63fd6dfe42c25ea28e97c34ef7cd5673c
Franz Király <fkiraly@gcos.ai>
2025-05-05 18:01:08 +0200

    Update __init__.py

--- src/hyperactive/experiment/toy/__init__.py
+++ src/hyperactive/experiment/toy/__init__.py
@@ -5,4 +5,8 @@ from hyperactive.experiment.toy._ackley import Ackley
 from hyperactive.experiment.toy._parabola import Parabola
 from hyperactive.experiment.toy._sphere import Sphere
 
-__all__ = ["Parabola"]
+__all__ = [
+    "Ackley",
+    "Parabola",
+    "Sphere",
+]
