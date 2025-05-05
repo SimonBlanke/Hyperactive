765f36acd2224564f8a8a0c847872aa81c5e047c
Franz Király <fkiraly@gcos.ai>
2025-05-05 20:33:58 +0200

    Update test_parametrize_with_checks.py

--- tests/integrations/sklearn/test_parametrize_with_checks.py
+++ tests/integrations/sklearn/test_parametrize_with_checks.py
@@ -4,15 +4,16 @@ from hyperactive.integrations import HyperactiveSearchCV, OptCV
 from hyperactive.opt import GridSearch
 from hyperactive.optimizers import RandomSearchOptimizer
 
+from sklearn.model_selection import KFold
 from sklearn.utils.estimator_checks import parametrize_with_checks
-
 
 svc = svm.SVC()
 parameters = {"kernel": ["linear", "rbf"], "C": [1, 10]}
 opt = RandomSearchOptimizer()
 hyperactivecv = HyperactiveSearchCV(svc, parameters, opt)
 
-optcv = OptCV(estimator=svc, optimizer=GridSearch(param_grid=parameters))
+cv = KFold(n_splits=2, shuffle=True, random_state=42)
+optcv = OptCV(estimator=svc, optimizer=GridSearch(param_grid=parameters, cv=cv))
 
 ESTIMATORS = [hyperactivecv, optcv]
 
