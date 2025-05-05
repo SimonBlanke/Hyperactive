55b60497d3672447b384aa73323426cd4489d9a1
Franz Király <fkiraly@gcos.ai>
2025-05-05 13:55:01 +0200

    Update __init__.py

--- src/hyperactive/opt/__init__.py
+++ src/hyperactive/opt/__init__.py
@@ -1,5 +1,9 @@
 """Individual optimization algorithms."""
 
+from hyperactive.gfo import HillClimbing
 from hyperactive.opt.gridsearch import GridSearch
 
-__all__ = ["GridSearch"]
+__all__ = [
+    "GridSearch",
+    "HillClimbing",
+]
