0fcb4cbc7a8484d228b72df4160b1f26caf7ee58
Franz Király <fkiraly@gcos.ai>
2025-05-24 09:25:48 +0200

    Update __init__.py

--- src/hyperactive/opt/__init__.py
+++ src/hyperactive/opt/__init__.py
@@ -1,11 +1,16 @@
 """Individual optimization algorithms."""
 # copyright: hyperactive developers, MIT License (see LICENSE file)
 
-from hyperactive.opt.hillclimbing import HillClimbing, StochasticHillClimbing
+from hyperactive.opt.hillclimbing import (
+    HillClimbing,
+    RepulsingHillClimbing,
+    StochasticHillClimbing,
+)
 from hyperactive.opt.gridsearch import GridSearch
 
 __all__ = [
     "GridSearch",
     "HillClimbing",
+    "RepulsingHillClimbing",
     "StochasticHillClimbing",
 ]
