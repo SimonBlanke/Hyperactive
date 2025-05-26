be65a9dfc7326e43ba76f19b3f6ed8d2e0a99365
Franz Király <fkiraly@gcos.ai>
2025-05-26 21:13:12 +0200

    Update __init__.py

--- src/hyperactive/opt/gfo/__init__.py
+++ src/hyperactive/opt/gfo/__init__.py
@@ -1,9 +1,9 @@
 """Optimizers from Gradient free optimizers package."""
 # copyright: hyperactive developers, MIT License (see LICENSE file)
 
-from hyperactive.opt.hillclimbing._hillclimbing import HillClimbing
-from hyperactive.opt.hillclimbing._repulsing import RepulsingHillClimbing
-from hyperactive.opt.hillclimbing._stochastic import StochasticHillClimbing
+from hyperactive.opt.gfo._hillclimbing import HillClimbing
+from hyperactive.opt.gfo._hillclimbing_repulsing import RepulsingHillClimbing
+from hyperactive.opt.gfo._hillclimbing_stochastic import StochasticHillClimbing
 
 __all__ = [
     "HillClimbing",
