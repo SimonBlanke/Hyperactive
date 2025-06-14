49210f2b5646080ef42c5639271db54829a3b41d
Simon Blanke <simon.blanke@yahoo.com>
2025-06-14 11:03:21 +0200

    add spiral opt.

--- src/hyperactive/opt/gfo/_spiral_optimization.py
+++ src/hyperactive/opt/gfo/_spiral_optimization.py
@@ -2,6 +2,65 @@ from hyperactive.opt._adapters._gfo import _BaseGFOadapter
 
 
 class SpiralOptimization(_BaseGFOadapter):
+    """Spiral optimizer.
+
+    Parameters
+    ----------
+    search_space : dict[str, list]
+        The search space to explore. A dictionary with parameter
+        names as keys and a numpy array as values.
+        Optional, can be passed later via ``set_params``.
+    initialize : dict[str, int], default={"grid": 4, "random": 2, "vertices": 4}
+        The method to generate initial positions. A dictionary with
+        the following key literals and the corresponding value type:
+        {"grid": int, "vertices": int, "random": int, "warm_start": list[dict]}
+    constraints : list[callable], default=[]
+        A list of constraints, where each constraint is a callable.
+        The callable returns `True` or `False` dependend on the input parameters.
+    random_state : None, int, default=None
+        If None, create a new random state. If int, create a new random state
+        seeded with the value.
+    rand_rest_p : float, default=0.1
+        The probability of a random iteration during the the search process.
+    population : int
+        The number of particles in the swarm.
+    decay_rate : float
+        This parameter is a factor, that influences the radius of the particles during their spiral movement.
+        Lower values accelerates the convergence of the particles to the best known position, while values above 1 eventually lead to a movement where the particles spiral away from each other.
+    n_iter : int, default=100
+        The number of iterations to run the optimizer.
+    verbose : bool, default=False
+        If True, print the progress of the optimization process.
+    experiment : BaseExperiment, optional
+        The experiment to optimize parameters for.
+        Optional, can be passed later via ``set_params``.
+    """
+
+    def __init__(
+        self,
+        search_space=None,
+        initialize=None,
+        constraints=None,
+        random_state=None,
+        rand_rest_p=0.1,
+        population: int = 10,
+        decay_rate: float = 0.99,
+        n_iter=100,
+        verbose=False,
+        experiment=None,
+    ):
+        self.random_state = random_state
+        self.rand_rest_p = rand_rest_p
+        self.population = population
+        self.decay_rate = decay_rate
+        self.search_space = search_space
+        self.initialize = initialize
+        self.constraints = constraints
+        self.n_iter = n_iter
+        self.experiment = experiment
+        self.verbose = verbose
+
+        super().__init__()
 
     def _get_gfo_class(self):
         """Get the GFO class to use.
@@ -14,3 +73,29 @@ class SpiralOptimization(_BaseGFOadapter):
         from gradient_free_optimizers import SpiralOptimization
 
         return SpiralOptimization
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Get the test parameters for the optimizer.
+
+        Returns
+        -------
+        dict with str keys
+            The test parameters dictionary.
+        """
+        import numpy as np
+
+        params = super().get_test_params()
+        experiment = params[0]["experiment"]
+        more_params = {
+            "experiment": experiment,
+            "population": 20,
+            "decay_rate": 0.9999,
+            "search_space": {
+                "C": np.array([0.01, 0.1, 1, 10]),
+                "gamma": np.array([0.0001, 0.01, 0.1, 1, 10]),
+            },
+            "n_iter": 100,
+        }
+        params.append(more_params)
+        return params
