5a8cac7380ffa2dbbbaac4da431d710153e94419
Simon Blanke <simon.blanke@yahoo.com>
2025-06-14 11:01:00 +0200

    add random search

--- src/hyperactive/opt/gfo/_random_search.py
+++ src/hyperactive/opt/gfo/_random_search.py
@@ -2,6 +2,52 @@ from hyperactive.opt._adapters._gfo import _BaseGFOadapter
 
 
 class RandomSearch(_BaseGFOadapter):
+    """Random search optimizer.
+
+    Parameters
+    ----------
+    search_space : dict[str, list]
+        The search space to explore. A dictionary with parameter
+        names as keys and a numpy array as values.
+        Optional, can be passed later via ``set_params``.
+    initialize : dict[str, int], default={"grid": 4, "random": 2, "vertices": 4}
+        The method to generate initial positions. A dictionary with
+        the following key literals and the corresponding value type:
+        {"grid": int, "vertices": int, "random": int, "warm_start": list[dict]}
+    constraints : list[callable], default=[]
+        A list of constraints, where each constraint is a callable.
+        The callable returns `True` or `False` dependend on the input parameters.
+    random_state : None, int, default=None
+        If None, create a new random state. If int, create a new random state
+        seeded with the value.
+    n_iter : int, default=100
+        The number of iterations to run the optimizer.
+    verbose : bool, default=False
+        If True, print the progress of the optimization process.
+    experiment : BaseExperiment, optional
+        The experiment to optimize parameters for.
+        Optional, can be passed later via ``set_params``.
+    """
+
+    def __init__(
+        self,
+        search_space=None,
+        initialize=None,
+        constraints=None,
+        random_state=None,
+        n_iter=100,
+        verbose=False,
+        experiment=None,
+    ):
+        self.random_state = random_state
+        self.search_space = search_space
+        self.initialize = initialize
+        self.constraints = constraints
+        self.n_iter = n_iter
+        self.experiment = experiment
+        self.verbose = verbose
+
+        super().__init__()
 
     def _get_gfo_class(self):
         """Get the GFO class to use.
@@ -14,3 +60,27 @@ class RandomSearch(_BaseGFOadapter):
         from gradient_free_optimizers import RandomSearchOptimizer
 
         return RandomSearchOptimizer
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Get the test parameters for the optimizer.
+
+        Returns
+        -------
+        dict with str keys
+            The test parameters dictionary.
+        """
+        import numpy as np
+
+        params = super().get_test_params()
+        experiment = params[0]["experiment"]
+        more_params = {
+            "experiment": experiment,
+            "search_space": {
+                "C": np.array([0.01, 0.1, 1, 10]),
+                "gamma": np.array([0.0001, 0.01, 0.1, 1, 10]),
+            },
+            "n_iter": 100,
+        }
+        params.append(more_params)
+        return params
