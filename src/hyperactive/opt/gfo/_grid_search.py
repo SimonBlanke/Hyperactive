7d758f0ad62a1d780321acff6494bc0a9b704567
Simon Blanke <simon.blanke@yahoo.com>
2025-06-14 10:44:31 +0200

    add grid search opt.

--- src/hyperactive/opt/gfo/_grid_search.py
+++ src/hyperactive/opt/gfo/_grid_search.py
@@ -2,6 +2,63 @@ from hyperactive.opt._adapters._gfo import _BaseGFOadapter
 
 
 class GridSearch(_BaseGFOadapter):
+    """Grid search optimizer.
+
+    Parameters
+    ----------
+    search_space : dict[str, list]
+        The search space to explore. A dictionary with parameter
+        names as keys and a numpy array as values.
+    initialize : dict[str, int]
+        The method to generate initial positions. A dictionary with
+        the following key literals and the corresponding value type:
+        {"grid": int, "vertices": int, "random": int, "warm_start": list[dict]}
+    constraints : list[callable]
+        A list of constraints, where each constraint is a callable.
+        The callable returns `True` or `False` dependend on the input parameters.
+    random_state : None, int
+        If None, create a new random state. If int, create a new random state
+        seeded with the value.
+    rand_rest_p : float
+        The probability of a random iteration during the the search process.
+    step_size : int
+        The step-size for the grid search.
+    direction : "diagonal" or "orthogonal"
+        The direction of the grid search.
+    n_iter : int, default=100
+        The number of iterations to run the optimizer.
+    verbose : bool, default=False
+        If True, print the progress of the optimization process.
+    experiment : BaseExperiment, optional
+        The experiment to optimize parameters for.
+        Optional, can be passed later via ``set_params``.
+    """
+
+    def __init__(
+        self,
+        search_space=None,
+        initialize=None,
+        constraints=None,
+        random_state=None,
+        rand_rest_p=0.1,
+        step_size=1,
+        direction="diagonal",
+        n_iter=100,
+        verbose=False,
+        experiment=None,
+    ):
+        self.random_state = random_state
+        self.rand_rest_p = rand_rest_p
+        self.step_size = step_size
+        self.direction = direction
+        self.search_space = search_space
+        self.initialize = initialize
+        self.constraints = constraints
+        self.n_iter = n_iter
+        self.experiment = experiment
+        self.verbose = verbose
+
+        super().__init__()
 
     def _get_gfo_class(self):
         """Get the GFO class to use.
@@ -14,3 +71,29 @@ class GridSearch(_BaseGFOadapter):
         from gradient_free_optimizers import GridSearchOptimizer
 
         return GridSearchOptimizer
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Get the test parameters for the optimizer.
+
+        Returns
+        -------
+        dict with str keys
+            The test parameters dictionary.
+        """
+        import numpy as np
+
+        params = super().get_test_params()
+        experiment = params[0]["experiment"]
+        more_params = {
+            "experiment": experiment,
+            "step_size": 3,
+            "direction": "orthogonal",
+            "search_space": {
+                "C": np.array([0.01, 0.1, 1, 10]),
+                "gamma": np.array([0.0001, 0.01, 0.1, 1, 10]),
+            },
+            "n_iter": 100,
+        }
+        params.append(more_params)
+        return params
