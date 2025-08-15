b5eee086580290ff0993d5a973ae3b83be5c0db6
Franz Király <fkiraly@gcos.ai>
2025-08-15 22:03:42 +0200

    rename, export

--- src/hyperactive/experiment/integrations/__init__.py
+++ src/hyperactive/experiment/integrations/__init__.py
@@ -3,5 +3,8 @@
 
 
 from hyperactive.experiment.integrations.sklearn_cv import SklearnCvExperiment
+from hyperactive.experiment.integrations.sktime_forecasting import (
+    SktimeForecastingExperiment,
+)
 
-__all__ = ["SklearnCvExperiment"]
+__all__ = ["SklearnCvExperiment", "SktimeForecastingExperiment"]
