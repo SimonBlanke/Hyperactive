f5184b89af482f5bc731a8fe8bf03c238961fb86
Franz Király <fkiraly@gcos.ai>
2025-08-15 22:25:25 +0200

    linting

--- src/hyperactive/experiment/integrations/__init__.py
+++ src/hyperactive/experiment/integrations/__init__.py
@@ -1,7 +1,6 @@
 """Integrations with packages for tuning."""
 # copyright: hyperactive developers, MIT License (see LICENSE file)
 
-
 from hyperactive.experiment.integrations.sklearn_cv import SklearnCvExperiment
 from hyperactive.experiment.integrations.sktime_forecasting import (
     SktimeForecastingExperiment,
