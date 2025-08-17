84b14c24e470aa89e32b03c81ac349f77a1ea3ba
Franz Király <fkiraly@gcos.ai>
2025-08-17 20:12:01 +0200

    sklearn tests

---
+++ src/hyperactive/integrations/sklearn/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for sklearn integrations."""
---
+++ src/hyperactive/integrations/sklearn/tests/test_parametrize_with_checks.py
@@ -0,0 +1,24 @@
+"""Test module for sklearn parametrize_with_checks integration."""
+
+from sklearn import svm
+from sklearn.model_selection import KFold
+from sklearn.utils.estimator_checks import parametrize_with_checks
+
+from hyperactive.integrations import OptCV
+from hyperactive.opt import GridSearchSk as GridSearch
+from hyperactive.optimizers import RandomSearchOptimizer
+
+svc = svm.SVC()
+parameters = {"kernel": ["linear", "rbf"], "C": [1, 10]}
+opt = RandomSearchOptimizer()
+
+cv = KFold(n_splits=2, shuffle=True, random_state=42)
+optcv = OptCV(estimator=svc, optimizer=GridSearch(param_grid=parameters), cv=cv)
+
+ESTIMATORS = [optcv]
+
+
+@parametrize_with_checks(ESTIMATORS)
+def test_estimators(estimator, check):
+    """Test estimators with sklearn estimator checks."""
+    check(estimator)
